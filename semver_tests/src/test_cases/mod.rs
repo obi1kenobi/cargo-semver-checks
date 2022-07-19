bd4d0a95c82de920447cbcbd4caccdec79127eb2
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-07-19 18:38:15 -0400

    Prevent overly broad rule matches on sub-item deletions.

--- semver_tests/src/test_cases/mod.rs
+++ semver_tests/src/test_cases/mod.rs
@@ -1,28 +1,14 @@
 pub mod non_exhaustive;
+pub mod struct_pub_field_missing;
+pub mod enum_variant_missing;
 
 /// Testing: <https://doc.rust-lang.org/cargo/reference/semver.html#item-remove>
 #[cfg(not(feature = "struct_missing"))]
 pub struct WillBeRemovedStruct;
 
-pub struct FieldWillBeRemoved {
-    pub foo: usize,
-
-    /// Testing: <https://doc.rust-lang.org/cargo/reference/semver.html#item-remove>
-    #[cfg(not(feature = "struct_pub_field_missing"))]
-    pub bar: usize,
-}
-
 /// Testing: <https://doc.rust-lang.org/cargo/reference/semver.html#item-remove>
 #[cfg(not(feature = "enum_missing"))]
 pub enum WillBeRemovedEnum {}
-
-pub enum VariantWillBeRemoved {
-    Foo,
-
-    /// Testing: <https://doc.rust-lang.org/cargo/reference/semver.html#item-remove>
-    #[cfg(not(feature = "enum_variant_missing"))]
-    Bar,
-}
 
 #[cfg(not(feature = "unit_struct_changed_kind"))]
 pub struct UnitStructToPlain;
