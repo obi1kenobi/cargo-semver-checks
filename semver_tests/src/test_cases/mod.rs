f882a66ada1a0b9cb11c21391d2513b23bc8e3c3
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-07-19 11:39:01 -0400

    Ensure the reported struct was not #[non_exhaustive].

--- semver_tests/src/test_cases/mod.rs
+++ semver_tests/src/test_cases/mod.rs
@@ -29,3 +29,11 @@ pub struct UnitStructToPlain;
 
 #[cfg(feature = "unit_struct_changed_kind")]
 pub struct UnitStructToPlain {}
+
+#[cfg(not(feature = "unit_struct_changed_kind"))]
+#[non_exhaustive]
+pub struct NonExhaustiveUnitStructToPlain;
+
+#[cfg(feature = "unit_struct_changed_kind")]
+#[non_exhaustive]
+pub struct NonExhaustiveUnitStructToPlain {}
