47a57b8a3e53a4ee812b51846a1352a2e25b318d
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-07-28 18:19:21 -0700

    Add type info to schema and use it in the query.

--- semver_tests/src/test_cases/struct_repr_transparent_removed.rs
+++ semver_tests/src/test_cases/struct_repr_transparent_removed.rs
@@ -16,6 +16,39 @@ pub struct Bar(pub usize);
 #[cfg(feature = "struct_repr_transparent_removed")]
 pub struct Bar(pub usize);
 
+#[cfg(not(feature = "struct_repr_transparent_removed"))]
+#[repr(transparent)]
+pub struct WithZeroSizedData<T> {
+    pub bar: usize,
+    _marker: std::marker::PhantomData<T>,
+}
+
+#[cfg(feature = "struct_repr_transparent_removed")]
+pub struct WithZeroSizedData<T> {
+    pub bar: usize,
+    _marker: std::marker::PhantomData<T>,
+}
+
+#[cfg(not(feature = "struct_repr_transparent_removed"))]
+#[repr(transparent)]
+pub struct TupleWithZeroSizedData<T>(pub usize, core::marker::PhantomData<T>);
+
+#[cfg(feature = "struct_repr_transparent_removed")]
+pub struct TupleWithZeroSizedData<T>(pub usize, core::marker::PhantomData<T>);
+
+#[cfg(not(feature = "struct_repr_transparent_removed"))]
+#[repr(transparent)]
+pub struct WithSpecificZeroSizedData {
+    pub bar: usize,
+    _marker: std::marker::PhantomData<&'static usize>,
+}
+
+#[cfg(feature = "struct_repr_transparent_removed")]
+pub struct WithSpecificZeroSizedData {
+    pub bar: usize,
+    _marker: std::marker::PhantomData<&'static usize>,
+}
+
 // Per https://doc.rust-lang.org/nomicon/other-reprs.html#reprtransparent
 // `repr(transparent)` is only part of the public ABI if the single non-zero-sized field
 // within the struct is public. In the following structs, the field is not public,
