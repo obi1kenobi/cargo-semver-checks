2664da7de0dffcba66a9bfca6ebab45f5df8cf50
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-08-04 10:45:10 -0700

    Check for changes in #[repr(i/usize)] enums as well.

--- semver_tests/src/test_cases/enum_repr_int_removed.rs
+++ semver_tests/src/test_cases/enum_repr_int_removed.rs
@@ -23,3 +23,29 @@ pub enum I32Enum {
     Bar,
     Baz,
 }
+
+#[cfg(not(feature = "enum_repr_int_removed"))]
+#[repr(isize)]
+pub enum IsizeEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_removed")]
+pub enum IsizeEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_removed"))]
+#[repr(usize)]
+pub enum UsizeEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_removed")]
+pub enum UsizeEnum {
+    Bar,
+    Baz,
+}
