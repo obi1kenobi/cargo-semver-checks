984b627f73fef9b137126c043cf8dcc54a41a160
Ed Page <eopage@gmail.com>
2022-08-18 09:40:45 -0500

    fix(cli): Give rustdoc flags more specific names
    `--baseline` and `--current` are less meaningful when there are multiple
    sources for those and we don't expect the rustdoc versions to be the
    most common.
    
    This soft-deprecates the old way, making it still work while hiding it.

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ fn main() -> anyhow::Result<()> {
     match args {
         SemverChecks::CheckRelease(args) => {
             let loader: Box<dyn baseline::BaselineLoader> =
-                if let Some(path) = args.baseline_rustdoc_path.as_deref() {
+                if let Some(path) = args.baseline_rustdoc.as_deref() {
                     Box::new(baseline::RustdocBaseline::new(path.to_owned()))
                 } else if let Some(root) = args.baseline_root.as_deref() {
                     Box::new(baseline::PathBaseline::new(root.to_owned())?)
@@ -70,35 +70,35 @@ fn main() -> anyhow::Result<()> {
                 };
             let rustdoc_cmd = dump::RustDocCommand::new().deps(false).silence(false);
 
-            let rustdoc_paths =
-                if let Some(current_rustdoc_path) = args.current_rustdoc_path.as_deref() {
-                    vec![(
-                        loader.load_rustdoc(&rustdoc_cmd, "<unknown>")?,
-                        current_rustdoc_path.to_owned(),
-                    )]
-                } else {
-                    let metadata = args.manifest.metadata().exec()?;
-                    let (selected, _) = args.workspace.partition_packages(&metadata);
-                    if selected.len() != 1 {
-                        anyhow::bail!(
-                            "only one package can be processed at a time: {}",
-                            selected
-                                .into_iter()
-                                .map(|s| s.name.clone())
-                                .collect::<Vec<_>>()
-                                .join(", ")
-                        );
-                    }
-                    let mut rustdoc_paths = Vec::with_capacity(selected.len());
-                    for selected in selected {
-                        let manifest_path = selected.manifest_path.as_std_path();
-                        let rustdoc_path = rustdoc_cmd.dump(manifest_path)?;
-                        let crate_name = manifest::get_package_name(manifest_path)?;
-                        let baseline_path = loader.load_rustdoc(&rustdoc_cmd, &crate_name)?;
-                        rustdoc_paths.push((baseline_path, rustdoc_path));
-                    }
-                    rustdoc_paths
-                };
+            let rustdoc_paths = if let Some(current_rustdoc_path) = args.current_rustdoc.as_deref()
+            {
+                vec![(
+                    loader.load_rustdoc(&rustdoc_cmd, "<unknown>")?,
+                    current_rustdoc_path.to_owned(),
+                )]
+            } else {
+                let metadata = args.manifest.metadata().exec()?;
+                let (selected, _) = args.workspace.partition_packages(&metadata);
+                if selected.len() != 1 {
+                    anyhow::bail!(
+                        "only one package can be processed at a time: {}",
+                        selected
+                            .into_iter()
+                            .map(|s| s.name.clone())
+                            .collect::<Vec<_>>()
+                            .join(", ")
+                    );
+                }
+                let mut rustdoc_paths = Vec::with_capacity(selected.len());
+                for selected in selected {
+                    let manifest_path = selected.manifest_path.as_std_path();
+                    let rustdoc_path = rustdoc_cmd.dump(manifest_path)?;
+                    let crate_name = manifest::get_package_name(manifest_path)?;
+                    let baseline_path = loader.load_rustdoc(&rustdoc_cmd, &crate_name)?;
+                    rustdoc_paths.push((baseline_path, rustdoc_path));
+                }
+                rustdoc_paths
+            };
             let mut success = true;
             for (baseline_path, current_path) in rustdoc_paths {
                 let baseline_crate = load_rustdoc_from_file(&baseline_path)?;
@@ -145,13 +145,14 @@ struct CheckRelease {
 
     /// The current rustdoc json output to test for semver violations.
     #[clap(
+        long,
         short_alias = 'c',
-        long = "current",
+        alias = "current",
         value_name = "JSON_PATH",
         help_heading = "CURRENT",
-        requires = "baseline-rustdoc-path"
+        requires = "baseline-rustdoc"
     )]
-    current_rustdoc_path: Option<PathBuf>,
+    current_rustdoc: Option<PathBuf>,
 
     /// Directory containing baseline crate source
     #[clap(
@@ -164,13 +165,14 @@ struct CheckRelease {
 
     /// The rustdoc json file to use as a semver baseline.
     #[clap(
+        long,
         short_alias = 'b',
-        long = "baseline",
+        alias = "baseline",
         value_name = "JSON_PATH",
         help_heading = "BASELINE",
         group = "baseline"
     )]
-    baseline_rustdoc_path: Option<PathBuf>,
+    baseline_rustdoc: Option<PathBuf>,
 }
 
 #[test]
