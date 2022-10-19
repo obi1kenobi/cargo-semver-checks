ddbd10ad7112fd3e232c7a4ff157c1dc3051e8af
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2022-10-20 00:05:05 +0200

    Fixed test

--- semver_tests/src/test_cases/enum_structvariant_field_missing.rs
+++ semver_tests/src/test_cases/enum_structvariant_field_missing.rs
@@ -1,19 +1,19 @@
 pub enum Enum {
     FieldWillBeMissing {
-        pub foo: usize,
+        foo: usize,
 
         /// Testing: <https://doc.rust-lang.org/cargo/reference/semver.html#item-remove>
         #[cfg(not(feature = "enum_structvariant_field_missing"))]
-        pub bar: usize,
+        bar: usize,
     }
 }
 
 /// This struct variant should not be reported by the `enum_structvariant_field_missing` rule:
 /// it will be removed altogether, so the correct rule to catch it is
 /// the `enum_variant_missing` rule and not the rule for missing fields.
-pub enum Enum {
+pub enum IgnoredEnum {
     #[cfg(not(feature = "enum_structvariant_field_missing"))]
     StructVariantWillBeMissing {
-        pub foo: usize,
+        foo: usize,
     }
 }
