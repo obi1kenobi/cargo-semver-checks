d64f7f6ec92e3b46210549a0483983689a7fd298
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-11-26 18:29:48 +0000

    Add multiple methods by the same name test case.

--- test_crates/src/test_cases/parameter_count_changed.rs
+++ test_crates/src/test_cases/parameter_count_changed.rs
@@ -77,3 +77,23 @@ impl PrivateStruct {
     #[cfg(feature = "method_parameter_count_changed")]
     pub fn method_with_a_parameter_added(_: ()) {}
 }
+
+// Ensure there are no false-positives in cases with
+// multiple methods with the same name but different signatures.
+// Here, the struct defines an inherent method `duplicate()`,
+// and also has `<Self as DuplicateMethodTrait>::duplicate()` available with a different signature.
+//
+// In https://github.com/obi1kenobi/cargo-semver-checks/issues/193 this sort of setup
+// matched the `method_parameter_count_changed` lint and was a false-positive.
+// It should obviously not match since no change happened.
+pub struct DuplicateMethodNames {}
+
+impl DuplicateMethodNames {
+    pub fn duplicate(&self) {}
+}
+
+trait DuplicateMethodTrait {
+    fn duplicate(&self, _: ()) {}
+}
+
+impl DuplicateMethodTrait for DuplicateMethodNames {}
