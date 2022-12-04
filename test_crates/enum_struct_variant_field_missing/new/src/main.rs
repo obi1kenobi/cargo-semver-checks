bd42fd97c58c079d49b021fc25adb1fa9bbfd8d8
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2022-12-04 21:07:58 +0100

    Removed some small stuff I missed during splitting

--- test_crates/enum_struct_variant_field_missing/new/src/main.rs
+++ test_crates/enum_struct_variant_field_missing/new/src/main.rs
@@ -4,7 +4,4 @@ pub enum Enum {
     }
 }
 
-/// This struct variant should not be reported by the `enum_struct_variant_field_missing` rule:
-/// it will be removed altogether, so the correct rule to catch it is
-/// the `enum_variant_missing` rule and not the rule for missing fields.
 pub enum IgnoredEnum {}
