5a661a050b8ddda46060c49b56f712fc65258d05
Bartosz Smolarczyk <bs429594@students.mimuw.edu.pl>
2022-12-10 20:49:04 +0100

    Strengthened enum_repr_c_removed tests

--- test_crates/src/test_cases/enum_repr_c_removed.rs
+++ test_crates/src/test_cases/enum_repr_c_removed.rs
@@ -1,35 +1,207 @@
 #[cfg(not(feature = "enum_repr_c_removed"))]
 #[repr(C)]
-pub enum Foo {
+pub enum CEnumToEnum {
     Bar,
 }
 
 #[cfg(feature = "enum_repr_c_removed")]
-pub enum Foo {
+pub enum CEnumToEnum {
     Bar,
+}
+
+#[cfg(not(feature = "enum_repr_c_removed"))]
+#[repr(u8, C)]
+pub enum U8CEnumToU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_c_removed")]
+#[repr(u8)]
+pub enum U8CEnumToU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_c_removed"))]
+#[repr(u8)]
+#[repr(C)]
+pub enum SeparateU8CEnumToU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_c_removed")]
+#[repr(u8)]
+pub enum SeparateU8CEnumToU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_c_removed"))]
+#[repr(C, u8)]
+pub enum CU8EnumToU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_c_removed")]
+#[repr(u8)]
+pub enum CU8EnumToU8Enum {
+    Bar,
+    Baz,
 }
 
 #[cfg(not(feature = "enum_repr_c_removed"))]
 #[repr(C)]
 #[repr(u8)]
-pub enum ComplexEquivalentReprEnum {
+pub enum SeparateCU8EnumToU8Enum {
     Bar,
-}
-
-#[cfg(feature = "enum_repr_c_removed")]
-#[repr(u8, C)]
-pub enum ComplexEquivalentReprEnum {
-    Bar,
-}
-
-#[cfg(not(feature = "enum_repr_c_removed"))]
-#[repr(u8, C)]
-pub enum ComplexRemovedReprEnum {
-    Bar,
+    Baz,
 }
 
 #[cfg(feature = "enum_repr_c_removed")]
 #[repr(u8)]
-pub enum ComplexRemovedReprEnum {
+pub enum SeparateCU8EnumToU8Enum {
     Bar,
+    Baz,
+}
+
+// The following enums have *rearrangements* of repr(C), potentially
+// splitting singular repr(*) into multiple, smaller repr(*) or merging
+// repr(*) into larger repr(*).
+// They should not be reported by this rule, because they are legal.
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(u8)]
+#[repr(C)]
+pub enum SeparateU8CToU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(u8, C)]
+pub enum SeparateU8CToU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(u8, C)]
+pub enum U8CToSeparateU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(u8)]
+#[repr(C)]
+pub enum U8CToSeparateU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(C)]
+#[repr(u8)]
+pub enum SeparateCU8ToCU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(C, u8)]
+pub enum SeparateCU8ToCU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(C, u8)]
+pub enum CU8ToSeparateCU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(C)]
+#[repr(u8)]
+pub enum CU8ToSeparateCU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(u8)]
+#[repr(C)]
+pub enum SeparateU8CToCU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(C, u8)]
+pub enum SeparateU8CToCU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(C)]
+#[repr(u8)]
+pub enum SeparateCU8ToU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(u8, C)]
+pub enum SeparateCU8ToU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(C, u8)]
+pub enum CU8ToU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(u8, C)]
+pub enum CU8ToU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(u8, C)]
+pub enum U8CToCU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(C, u8)]
+pub enum U8CToCU8Enum {
+    Bar,
+    Baz,
+}
+
+#[cfg(not(feature = "enum_repr_int_changed"))]
+#[repr(C)]
+#[repr(u8)]
+pub enum SeparateCU8ToSeparateU8CEnum {
+    Bar,
+    Baz,
+}
+
+#[cfg(feature = "enum_repr_int_changed")]
+#[repr(u8)]
+#[repr(C)]
+pub enum SeparateCU8ToSeparateU8CEnum {
+    Bar,
+    Baz,
 }
