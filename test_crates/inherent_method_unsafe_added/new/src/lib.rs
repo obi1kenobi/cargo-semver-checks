c756bc2ec205e2b05c688374962d6202f80b9df4
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2022-12-16 21:27:14 -0500

    Add lints for adding `unsafe` to methods and functions. (#226)
    Resolves #191.

---
+++ test_crates/inherent_method_unsafe_added/new/src/lib.rs
@@ -0,0 +1,11 @@
+pub struct Foo;
+
+impl Foo {
+    pub unsafe fn associated_fn(x: i64, y: i64) -> i64 {
+        x + y
+    }
+
+    pub unsafe fn method(&self, x: i64) -> i64 {
+        x
+    }
+}
