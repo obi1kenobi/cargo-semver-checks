f950f99e02f7de6c7bd1b5aafda4f6197e9c6122
Bartosz Smolarczyk <bs429594@students.mimuw.edu.pl>
2023-01-09 23:17:51 +0100

    New test crate: inherent_method_moved_must_use_added

--- test_crates/inherent_method_moved_must_use_added/new/src/lib.rs
+++ test_crates/inherent_method_moved_must_use_added/new/src/lib.rs
@@ -1,40 +1,3 @@
-pub struct StructWithMustUseMethods {}
-
-impl StructWithMustUseMethods {
-
-    // These methods did not have the #[must_use] attribute in the old version.
-    // Addition of the attribute should be reported.
-
-    #[must_use]
-    pub fn MethodToMustUseMethod(&self) {}
-
-    #[must_use = "Foo"]
-    pub fn MethodToMustUseMessageMethod(&self) {}
-
-
-    // These methods had the #[must_use] attribute in the old version. Changes of
-    // the attribute, including deletion, should not be reported.
-
-    pub fn MustUseMethodToMethod(&self) {}
-
-    #[must_use = "Foo"]
-    pub fn MustUseMethodToMustUseMessageMethod(&self) {}
-
-
-    // These methods had the #[must_use] attribute in the old version.
-    // They also included the user-defined warning message. Changes of
-    // the attribute, including deletion, should not be reported.
-
-    pub fn MustUseMessageMethodToMethod(&self) {}
-
-    #[must_use]
-    pub fn MustUseMessageMethodToMustUseMethod(&self) {}
-
-    #[must_use = "Baz"]
-    pub fn MustUseMessageMethodToMustUseMessageMethod(&self) {}
-}
-
-
 pub struct StructWithMovedProvidedMustUseMethods {}
 
 pub trait TraitWithMovedProvidedMustUseMethods {
@@ -128,89 +91,6 @@ impl TraitWithMovedDeclaredMustUseMethods for StructWithMovedDeclaredMustUseMeth
 }
 
 
-pub trait TraitWithDeclaredMustUseMethods {
-
-    // These methods did not have the #[must_use] attribute in the old version.
-    // Addition of the attribute should be reported.
-
-    #[must_use]
-    fn DeclaredMethodToDeclaredMustUseMethod(&self);
-
-    #[must_use = "Foo"]
-    fn DeclaredMethodToDeclaredMustUseMessageMethod(&self);
-
-
-    // These methods had the #[must_use] attribute in the old version. Changes of
-    // the attribute, including deletion, should not be reported.
-
-    fn DeclaredMustUseMethodToDeclaredMethod(&self);
-
-    #[must_use = "Foo"]
-    fn DeclaredMustUseMethodToDeclaredMustUseMessageMethod(&self);
-
-
-    // These methods had the #[must_use] attribute in the old version.
-    // They also included the user-defined warning message. Changes of
-    // the attribute, including deletion, should not be reported.
-
-    fn DeclaredMustUseMessageMethodToDeclaredMethod(&self);
-
-    #[must_use]
-    fn DeclaredMustUseMessageMethodToDeclaredMustUseMethod(&self);
-
-    #[must_use = "Baz"]
-    fn DeclaredMustUseMessageMethodToDeclaredMustUseMessageMethod(&self);
-}
-
-
-pub trait TraitWithProvidedMustUseMethods {
-
-    // These methods did not have the #[must_use] attribute in the old version.
-    // Addition of the attribute should be reported.
-
-    #[must_use]
-    fn ProvidedMethodToProvidedMustUseMethod(&self) {}
-
-    #[must_use = "Foo"]
-    fn ProvidedMethodToProvidedMustUseMessageMethod(&self) {}
-
-
-    // These methods had the #[must_use] attribute in the old version. Changes of
-    // the attribute, including deletion, should not be reported.
-
-    fn ProvidedMustUseMethodToProvidedMethod(&self) {}
-
-    #[must_use = "Foo"]
-    fn ProvidedMustUseMethodToProvidedMustUseMessageMethod(&self) {}
-
-
-    // These methods had the #[must_use] attribute in the old version.
-    // They also included the user-defined warning message. Changes of
-    // the attribute, including deletion, should not be reported.
-
-    fn ProvidedMustUseMessageMethodToProvidedMethod(&self) {}
-
-    #[must_use]
-    fn ProvidedMustUseMessageMethodToProvidedMustUseMethod(&self) {}
-
-    #[must_use = "Baz"]
-    fn ProvidedMustUseMessageMethodToProvidedMustUseMessageMethod(&self) {}
-}
-
-
-// This public struct's inherent method did not have the #[must_use] attribute
-// in the old version. Because the method is private, adding the attribute
-// should NOT be reported.
-
-pub struct StructWithPrivateMustUseMethods {}
-
-impl StructWithPrivateMustUseMethods {
-
-    #[must_use]
-    fn PrivateMethodToPrivateMustUseMethod(&self) {}
-}
-
-
 // This struct is private and adding #[must_use] to it's methods,
 // after moving them to a private trait, should NOT be reported.
 
@@ -233,19 +113,6 @@ impl PrivateTraitWithMovedMustUseMethods for PrivateStructWithMovedMustUseMethod
     fn PrivateStructMethodToMovedPrivateDeclaredMustUseMethod(&self) {}
 
     fn PrivateStructMethodToMovedPrivateOverrideMustUseMethod(&self) {}
-}
-
-
-// This trait is private and adding #[must_use] to it's methods
-// should NOT be reported.
-
-trait PrivateTraitWithMustUseMethods {
-
-    #[must_use]
-    fn PrivateDeclaredMethodToPrivateDeclaredMustUseMethod(&self);
-
-    #[must_use]
-    fn PrivateProvidedMethodToPrivateProvidedMustUseMethod(&self) {}
 }
 
 
@@ -272,19 +139,6 @@ impl TraitWithMovedImplMustUseMethods for StructWithMovedImplMustUseMethods {
 }
 
 
-// This struct and it's inherent method were added in the new version of the
-// crate, together with the method's attribute. It should NOT be reported
-// because adding a new struct is not a breaking change.
-
-pub struct NewStructWithMustUseMethods {}
-
-impl NewStructWithMustUseMethods {
-
-    #[must_use]
-    pub fn NewStructMustUseMethod(&self) {}
-}
-
-
 // This struct, trait and it's inherent methods were added in the new version of
 // the crate, together with the method's attribute. It should NOT be reported
 // because adding a new struct or trait is not a breaking change.
@@ -308,18 +162,4 @@ impl NewTraitWithStructMustUseMethods for NewStructWithTraitMustUseMethods {
     fn NewTraitWithStructDeclaredMustUseMethod(&self) {}
 
     fn NewTraitWithStructOverrideMustUseMethod(&self) {}
-}
-
-
-// This trait and it's methods were added in the new version of the crate,
-// together with the method's attribute. It should NOT be reported because
-// adding a new trait is not a breaking change.
-
-pub trait NewTraitWithMustUseMethods {
-
-    #[must_use]
-    fn NewTraitWithDeclaredMustUseMethod(&self);
-
-    #[must_use]
-    fn NewTraitWithProvidedMustUseMethod(&self) {}
 }
