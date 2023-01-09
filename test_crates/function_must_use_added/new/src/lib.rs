96a134bbfd9d5d088d846703b07170323561f6e5
Bartosz Smolarczyk <bs429594@students.mimuw.edu.pl>
2023-01-09 18:50:37 +0100

    Simpler functions in function_must_use_added.

--- test_crates/function_must_use_added/new/src/lib.rs
+++ test_crates/function_must_use_added/new/src/lib.rs
@@ -2,51 +2,35 @@
 // Addition of the attribute should be reported by this rule.
 
 #[must_use]
-pub fn FunctionToMustUseFunction(x: i64) -> i64 {
-    x
-}
+pub fn FunctionToMustUseFunction() {}
 
 #[must_use = "Foo"]
-pub fn FunctionToMustUseMessageFunction(x: i64) -> i64 {
-    x
-}
+pub fn FunctionToMustUseMessageFunction() {}
 
 
 // These functions had the #[must_use] attribute in the old version. Changes of
 // the attribute, including deletion, should not be reported by this rule.
 
-pub fn MustUseFunctionToFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseFunctionToFunction() {}
 
 #[must_use = "Foo"]
-pub fn MustUseFunctionToMustUseMessageFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseFunctionToMustUseMessageFunction() {}
 
 
 // These functions had the #[must_use] attribute in the old version.
 // They also included the user-defined warning message. Changes of
 // the attribute, including deletion, should not be reported by this rule.
 
-pub fn MustUseMessageFunctionToFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseMessageFunctionToFunction() {}
 
 #[must_use]
-pub fn MustUseMessageFunctionToMustUseFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseMessageFunctionToMustUseFunction() {}
 
 #[must_use = "Baz"]
-pub fn MustUseMessageFunctionToMustUseMessageFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseMessageFunctionToMustUseMessageFunction() {}
 
 
 // This function is private and should not be reported by this rule.
 
 #[must_use]
-fn MustUsePrivateFunction(x: i64) -> i64 {
-    x
-}
+fn MustUsePrivateFunction() {}
--- test_crates/function_must_use_added/old/src/lib.rs
+++ test_crates/function_must_use_added/old/src/lib.rs
@@ -1,27 +1,19 @@
 // These functions did not have the #[must_use] attribute in the old version.
 // Addition of the attribute should be reported by this rule.
 
-pub fn FunctionToMustUseFunction(x: i64) -> i64 {
-    x
-}
+pub fn FunctionToMustUseFunction() {}
 
-pub fn FunctionToMustUseMessageFunction(x: i64) -> i64 {
-    x
-}
+pub fn FunctionToMustUseMessageFunction() {}
 
 
 // These functions had the #[must_use] attribute in the old version. Changes of
 // the attribute, including deletion, should not be reported by this rule.
 
 #[must_use]
-pub fn MustUseFunctionToFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseFunctionToFunction() {}
 
 #[must_use]
-pub fn MustUseFunctionToMustUseMessageFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseFunctionToMustUseMessageFunction() {}
 
 
 // These functions had the #[must_use] attribute in the old version.
@@ -29,23 +21,15 @@ pub fn MustUseFunctionToMustUseMessageFunction(x: i64) -> i64 {
 // the attribute, including deletion, should not be reported by this rule.
 
 #[must_use = "Foo"]
-pub fn MustUseMessageFunctionToFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseMessageFunctionToFunction() {}
 
 #[must_use = "Foo"]
-pub fn MustUseMessageFunctionToMustUseFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseMessageFunctionToMustUseFunction() {}
 
 #[must_use = "Foo"]
-pub fn MustUseMessageFunctionToMustUseMessageFunction(x: i64) -> i64 {
-    x
-}
+pub fn MustUseMessageFunctionToMustUseMessageFunction() {}
 
 
 // This function is private and should not be reported by this rule.
 
-fn MustUsePrivateFunction(x: i64) -> i64 {
-    x
-}
+fn MustUsePrivateFunction() {}
