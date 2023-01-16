f825136168cc0b6a28013b7331351ab5517e6579
Bartosz Smolarczyk <92160712+SmolSir@users.noreply.github.com>
2023-01-16 01:01:08 +0100

    New lint: Inherent method `#[must_use]` added (#283)
    * New lint: inherent_method_must_use_added
    
    * Self-reviewed some comments.
    
    * Missing comma in query.rs
    
    * Additional tests for enum, union & item type changed.
    
    * Update test outputs for enum_missing & struct_missing
    
    * Update output for inherent_method_must_use_added
    
    * Add `publish = false` to Cargo.toml
    
    * Lint & update of output file.
    
    * Update src/lints/inherent_method_must_use_added.ron
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Bartosz Smolarczyk <bs429594@students.mimuw.edu.pl>
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>

---
+++ test_crates/inherent_method_must_use_added/old/src/lib.rs
@@ -0,0 +1,10 @@
+// This crate's test cases were separated into smaller files for easier
+// management and debugging. 
+
+pub mod enum_inherent_method_must_use_added;
+
+pub mod struct_inherent_method_must_use_added;
+
+pub mod union_inherent_method_must_use_added;
+
+pub mod item_type_changed_inherent_method_must_use_added;
