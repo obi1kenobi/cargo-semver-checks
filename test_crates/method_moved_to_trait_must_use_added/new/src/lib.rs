5c92270ed4ce2e9034afbfdcd99fc9408129ff99
Bartosz Smolarczyk <92160712+SmolSir@users.noreply.github.com>
2023-01-20 14:56:45 +0100

    New test crate: method moved to trait `#[must_use]` added (#282)
    * New test crate: method_moved_to_trait_must_use_added
    
    * Self-reviewed some comments.
    
    * Tiny change of alignment.
    
    * Typo in a comment.
    
    * Prepare files for test cases.
    
    * struct_method_moved_to_trait_must_use_added
    
    * enum_method_moved_to_trait_must_use_added
    
    * union_method_moved_to_trait_must_use_added
    
    * `publish = false`.
    
    Co-authored-by: Bartosz Smolarczyk <bs429594@students.mimuw.edu.pl>

---
+++ test_crates/method_moved_to_trait_must_use_added/new/src/lib.rs
@@ -0,0 +1,16 @@
+// The purpose of this test crate is to avoid duplicate lints. In this crate,
+// there could be reports about both #[must_use] being added to the methods,
+// as well as the methods themselves being moved to a Trait. 
+// Because both of these are minor changes, and the #[must_use] added violation
+// is impossible to achieve here without the method_moved_to_trait check failing,
+// we want the #[must_use] checks to not find any changes, but expect a failure
+// on the method_moved_to_trait check.
+
+// This crate's test cases were separated into smaller files for easier
+// management and debugging. 
+
+pub mod enum_method_moved_to_trait_must_use_added;
+
+pub mod struct_method_moved_to_trait_must_use_added;
+
+pub mod union_method_moved_to_trait_must_use_added;
