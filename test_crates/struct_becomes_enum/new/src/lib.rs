c36aac8b6f2668e12a3759ab0867831c3b00d22d
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2023-01-20 13:12:35 -0500

    Non-constructible struct becoming an enum is not per se breaking. (#299)

---
+++ test_crates/struct_becomes_enum/new/src/lib.rs
@@ -0,0 +1,22 @@
+/// Changing a struct that isn't externally-constructible (e.g. due to private fields)
+/// into an enum or union is not a breaking change by itself.
+///
+/// If all of the struct's methods and trait impls continue to exist,
+/// the change is not externally noticeable.
+///
+/// The new enum is even allowed to become `#[non_exhaustive]`
+/// without that being a breaking change.
+#[non_exhaustive]
+pub enum StructToEnum {
+    Var(i64),
+}
+
+impl StructToEnum {
+    pub fn new(x: i64) -> Self {
+        Self::Var(x)
+    }
+
+    pub fn present_associated_fn() {}
+
+    pub fn present_method(&self) {}
+}
