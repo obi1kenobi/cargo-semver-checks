7d83f9ab34d991c64b5ed1f441909539f0792e82
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-01-20 19:00:47 +0000

    Another tricky edge case for nonbreaking struct to enum conversions.

--- test_crates/struct_becomes_enum/new/src/lib.rs
+++ test_crates/struct_becomes_enum/new/src/lib.rs
@@ -1,11 +1,12 @@
-/// Changing a struct that isn't externally-constructible (e.g. due to private fields)
-/// into an enum or union is not a breaking change by itself.
-///
-/// If all of the struct's methods and trait impls continue to exist,
-/// the change is not externally noticeable.
-///
-/// The new enum is even allowed to become `#[non_exhaustive]`
-/// without that being a breaking change.
+//! Changing a struct that isn't externally-constructible (e.g. due to private fields)
+//! into an enum or union is not a breaking change by itself.
+//!
+//! If all of the struct's methods and trait impls continue to exist,
+//! the change is not externally noticeable.
+//!
+//! The new enum is even allowed to become `#[non_exhaustive]`
+//! without that being a breaking change.
+
 #[non_exhaustive]
 pub enum StructToEnum {
     Var(i64),
@@ -16,6 +17,23 @@ impl StructToEnum {
         Self::Var(x)
     }
 
+    pub fn present_associated_fn() {}
+
+    pub fn present_method(&self) {}
+}
+
+
+/// This struct is also not externally-constructible, due to `#[non_exhaustive]`.
+/// Changing it to an enum is also not a breaking change.
+///
+/// If this struct had any public fields, the loss of those fields in the conversion to enum
+/// would have been a breaking change. But as-is, this is not breaking.
+#[non_exhaustive]
+pub enum NonExhaustiveEmptyStructToEnum {
+    Var,
+}
+
+impl NonExhaustiveEmptyStructToEnum {
     pub fn present_associated_fn() {}
 
     pub fn present_method(&self) {}
--- test_crates/struct_becomes_enum/old/src/lib.rs
+++ test_crates/struct_becomes_enum/old/src/lib.rs
@@ -1,11 +1,12 @@
-/// Changing a struct that isn't externally-constructible (e.g. due to private fields)
-/// into an enum or union is not a breaking change by itself.
-///
-/// If all of the struct's methods and trait impls continue to exist,
-/// the change is not externally noticeable.
-///
-/// The new enum is even allowed to become `#[non_exhaustive]`
-/// without that being a breaking change.
+//! Changing a struct that isn't externally-constructible (e.g. due to private fields)
+//! into an enum or union is not a breaking change by itself.
+//!
+//! If all of the struct's methods and trait impls continue to exist,
+//! the change is not externally noticeable.
+//!
+//! The new enum is even allowed to become `#[non_exhaustive]`
+//! without that being a breaking change.
+
 pub struct StructToEnum {
     x: i64
 }
@@ -22,4 +23,19 @@ impl StructToEnum {
     pub fn will_be_removed_associated_fn() {}
 
     pub fn will_be_removed_method(&self) {}
+}
+
+
+/// This struct is also not externally-constructible, due to `#[non_exhaustive]`.
+/// Changing it to an enum is also not a breaking change.
+///
+/// If this struct had any public fields, the loss of those fields in the conversion to enum
+/// would have been a breaking change. But as-is, this is not breaking.
+#[non_exhaustive]
+pub struct NonExhaustiveEmptyStructToEnum {}
+
+impl NonExhaustiveEmptyStructToEnum {
+    pub fn present_associated_fn() {}
+
+    pub fn present_method(&self) {}
 }
