86da5eef6f51d9d090859453564e526a9c7e6ead
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2023-01-20 20:09:40 -0500

    Improve the test coverage for structs that change to enums. (#307)

--- test_crates/struct_becomes_enum/new/src/lib.rs
+++ test_crates/struct_becomes_enum/new/src/lib.rs
@@ -7,6 +7,55 @@
 //! The new enum is even allowed to become `#[non_exhaustive]`
 //! without that being a breaking change.
 
+
+// The following structs cannot change to enums with a non-breaking change
+// since their pub fields will become inaccessible.
+
+pub enum PubFieldTuple {
+    Var(i64, bool),
+}
+
+pub enum PubFieldStruct {
+    Var(i64, bool),
+}
+
+// Constructible fieldless structs cannot change to enums with a non-breaking change
+// due to the inability to construct the struct using a struct literal.
+//
+// Normally, this case is subordinate to the breaking change regarding the inability
+// to access pub fields. However, if the struct is exhaustive and has *no fields at all*
+// (e.g. a unit or empty tuple struct), then it is still externally-constructible.
+
+pub enum ConstructibleFieldlessUnit {}
+
+pub enum ConstructibleFieldlessTuple {
+    Var,
+}
+
+pub enum ConstructibleFieldlessStruct {}
+
+
+// The following structs are not externally-constructible due to their private fields.
+// They have no pub fields that might become inaccessible,
+// so they can become enums with a non-breaking change.
+//
+// They are even allowed to become non-exhaustive without that being a breaking change, either.
+
+pub enum TupleToEnum {
+    Var(i64)
+}
+
+impl TupleToEnum {
+    pub fn new(x: i64) -> Self {
+        Self::Var(x)
+    }
+
+    pub fn present_associated_fn() {}
+
+    pub fn present_method(&self) {}
+}
+
+
 #[non_exhaustive]
 pub enum StructToEnum {
     Var(i64),
@@ -37,4 +86,16 @@ impl NonExhaustiveEmptyStructToEnum {
     pub fn present_associated_fn() {}
 
     pub fn present_method(&self) {}
+}
+
+// The following structs are also not externally-constructible due to `#[non_exhaustive]`.
+
+#[non_exhaustive]
+pub enum NoneExhaustiveFieldlessUnit {
+    Var,
+}
+
+#[non_exhaustive]
+pub enum NonExhaustiveFieldlessTuple {
+    Var,
 }
--- test_crates/struct_becomes_enum/old/src/lib.rs
+++ test_crates/struct_becomes_enum/old/src/lib.rs
@@ -7,6 +7,55 @@
 //! The new enum is even allowed to become `#[non_exhaustive]`
 //! without that being a breaking change.
 
+
+// The following structs cannot change to enums with a non-breaking change
+// since their pub fields will become inaccessible.
+
+pub struct PubFieldTuple(pub i64, bool);
+
+pub struct PubFieldStruct {
+    pub x: i64,
+    _hidden: bool,
+}
+
+// Constructible fieldless structs cannot change to enums with a non-breaking change
+// due to the inability to construct the struct using a struct literal.
+//
+// Normally, this case is subordinate to the breaking change regarding the inability
+// to access pub fields. However, if the struct is exhaustive and has *no fields at all*
+// (e.g. a unit or empty tuple struct), then it is still externally-constructible.
+
+pub struct ConstructibleFieldlessUnit;
+
+pub struct ConstructibleFieldlessTuple();
+
+pub struct ConstructibleFieldlessStruct {}
+
+
+// The following structs are not externally-constructible due to their private fields.
+// They have no pub fields that might become inaccessible,
+// so they can become enums with a non-breaking change.
+//
+// They are even allowed to (but not required to) become non-exhaustive without
+// that being a breaking change, either.
+
+pub struct TupleToEnum(i64);
+
+impl TupleToEnum {
+    pub fn new(x: i64) -> Self {
+        Self(x)
+    }
+
+    pub fn present_associated_fn() {}
+
+    pub fn present_method(&self) {}
+
+    pub fn will_be_removed_associated_fn() {}
+
+    pub fn will_be_removed_method(&self) {}
+}
+
+
 pub struct StructToEnum {
     x: i64
 }
@@ -39,3 +88,11 @@ impl NonExhaustiveEmptyStructToEnum {
 
     pub fn present_method(&self) {}
 }
+
+// The following structs are also not externally-constructible due to `#[non_exhaustive]`.
+
+#[non_exhaustive]
+pub struct NoneExhaustiveFieldlessUnit;
+
+#[non_exhaustive]
+pub struct NonExhaustiveFieldlessTuple();
