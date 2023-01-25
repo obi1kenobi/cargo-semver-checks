b27b7ab42767f660019b4e6e03e1c3aa66406eb3
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-01-25 22:19:51 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -85,8 +85,7 @@ impl Rustdoc {
                 anyhow::bail!("not supported yet")
             }
         };
-        let manifest = manifest_from_dir(manifest_dir);
-        let metadata = manifest_metadata_no_deps(&manifest)?;
+        let metadata = manifest_metadata_no_deps(manifest_dir)?;
         let target = metadata.target_directory.as_std_path().join(util::SCOPE);
         let registry = baseline::RegistryBaseline::new(&target, config)?;
         Ok(registry)
