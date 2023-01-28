5f1e43f10de9d03057c2d7605cb4b5e8221ff2dc
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2023-01-27 19:06:55 -0500

    Add a test case with infinite importable paths. (#287)
    * Add a test case with infinite importable paths.
    
    * Make one more import public.
    
    * Make the test crates `publish = false`.

---
+++ test_crates/infinite_importable_paths/new/src/lib.rs
@@ -0,0 +1,23 @@
+//! This package exports the functions:
+//! - `foo`
+//! - `nested::foo`
+//! - `nested::nested::foo`
+//! etc. with arbitrarily many repetitions of `nested` allowed.
+//!
+//! This package uses a different way of achieving the same thing
+//! compared to its variant in the `old` directory.
+//! No semver issues should be reported when comparing the two.
+
+mod nested_other {
+    pub use super::nested;
+
+    pub fn foo() {}
+}
+
+pub mod nested {
+    pub use crate::nested_other::foo;
+
+    pub use crate::nested_other::nested;
+}
+
+pub use nested::foo;
