5433f494b46e5de711c8ded6890bd06a55d8ee68
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2023-02-04 15:31:29 -0500

    Add a test case for non-breaking item renaming. (#349)

---
+++ test_crates/nonbreaking_item_rename/old/src/lib.rs
@@ -0,0 +1,13 @@
+mod internal {
+    pub struct OldName;
+
+    pub enum OldEnum {
+        Foo,
+    }
+
+    pub trait OldTrait {}
+
+    pub fn old_fn() {}
+}
+
+pub use internal::{OldName, OldEnum, OldTrait, old_fn};
