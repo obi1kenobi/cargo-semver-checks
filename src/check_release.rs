14e77ee491733423d731b0e7ae62b08948d28514
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2023-02-09 22:39:23 +0100

    Fixed link in check_release.rs (#358)

--- src/check_release.rs
+++ src/check_release.rs
@@ -265,7 +265,7 @@ pub(super) fn run_check_release(
                         ref_link,
                         "impl:",
                         format!(
-                            "https://github.com/obi1kenobi/cargo-semver-check/tree/v{}/src/queries/{}.ron",
+                            "https://github.com/obi1kenobi/cargo-semver-check/tree/v{}/src/lints/{}.ron",
                             crate_version!(),
                             semver_query.id,
                         )
@@ -282,7 +282,7 @@ pub(super) fn run_check_release(
                         &semver_query.error_message,
                         "impl:",
                         format!(
-                            "https://github.com/obi1kenobi/cargo-semver-check/tree/v{}/src/queries/{}.ron",
+                            "https://github.com/obi1kenobi/cargo-semver-check/tree/v{}/src/lints/{}.ron",
                             crate_version!(),
                             semver_query.id,
                         )
