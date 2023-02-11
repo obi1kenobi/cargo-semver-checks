c4d52722b872235c654896599d2f5bb87882da14
Said Aroua <said.aroua@bluewin.ch>
2023-02-11 20:41:28 +0100

    Add tests for deprecated enums

--- test_crates/enum_struct_union_deprecated_added/new/src/lib.rs
+++ test_crates/enum_struct_union_deprecated_added/new/src/lib.rs
@@ -1,3 +1,5 @@
+// STRUCTS
+
 // These structs did not have the #[deprecated] attribute in the old version.
 // Addition of the attribute should be reported by this rule.
 
@@ -6,7 +8,7 @@ pub struct StructToDeprecatedStruct {
     bar: u64,
 }
 
-#[deprecated = "Foo"]
+#[deprecated = "This attribute was added"]
 pub struct StructToDeprecatedMessageStruct {
     bar: u64,
 }
@@ -55,5 +57,64 @@ struct DeprecatedPrivateStruct {
 
 #[deprecated]
 pub struct DeprecatedNewStruct {
+    bar: u64,
+}
+
+// ENUMS
+
+// These enums did not have the #[deprecated] attribute in the old version.
+// Addition of the attribute should be reported.
+
+#[deprecated]
+pub enum EnumToDeprecatedEnum {
+    First,
+}
+
+#[deprecated = "This attribute was added"]
+pub enum EnumToDeprecatedMessageEnum {
+    First,
+}
+
+// These structs had the #[deprecated] attribute in the old version.
+// They also included the user-defined warning message. Changes of
+// the attribute, including deletion, should NOT be reported by this rule.
+
+pub enum DeprecatedEnumToEnum {
+    First,
+}
+
+#[deprecated = "This message was added"]
+pub enum DeprecatedEnumToDeprecatedMessageEnum {
+    First,
+}
+
+pub enum DeprecatedMessageEnumToEnum {
+    First,
+}
+
+#[deprecated]
+pub enum DeprecatedMessageEnumToDeprecatedEnum {
+    First,
+}
+
+#[deprecated = "This message was changed"]
+pub enum DeprecatedMessageEnumToDeprecatedMessageEnum {
+    First,
+}
+
+// This enum is private and should NOT be reported.
+
+#[deprecated]
+enum DeprecatedPrivateEnum {
+    First,
+}
+
+// This struct was added in the new version of the crate with its attribute.
+// It should NOT be reported by this rule to avoid duplicate lints.
+// It should be reported as a new pub type that is part of the crate's API.
+// This might seem like a nonsensical test but is a valid edge case.
+
+#[deprecated]
+pub struct DeprecatedNewEnum {
     bar: u64,
 }
--- test_crates/enum_struct_union_deprecated_added/old/src/lib.rs
+++ test_crates/enum_struct_union_deprecated_added/old/src/lib.rs
@@ -10,7 +10,7 @@ pub struct StructToDeprecatedMessageStruct {
     bar: u64,
 }
 
-// These structs already have the attribute and changes to the attribute will not be reported.
+// These structs already have the attribute and changes to the attribute should not be reported.
 
 #[deprecated]
 pub struct DeprecatedStructToStruct {
@@ -41,4 +41,49 @@ pub struct DeprecatedMessageStructToDeprecatedMessageStruct {
 
 struct DeprecatedPrivateStruct {
     bar: u64,
+}
+
+// ENUMS
+
+// Adding #[deprecated] to these enums should be reported.
+
+pub enum EnumToDeprecatedEnum {
+    First,
+}
+
+pub enum EnumToDeprecatedMessageEnum {
+    First,
+}
+
+// These enums already have the attribute and changes to the attribute should not be reported.
+
+#[deprecated]
+pub enum DeprecatedEnumToEnum {
+    First,
+}
+
+#[deprecated]
+pub enum DeprecatedEnumToDeprecatedMessageEnum {
+    First,
+}
+
+#[deprecated = "This attribute will be deleted"]
+pub enum DeprecatedMessageEnumToEnum {
+    First,
+}
+
+#[deprecated = "This message will be deleted"]
+pub enum DeprecatedMessageEnumToDeprecatedEnum {
+    First,
+}
+
+#[deprecated = "This message will change"]
+pub enum DeprecatedMessageEnumToDeprecatedMessageEnum {
+    First,
+}
+
+// This enum is private and should NOT be reported.
+
+enum DeprecatedPrivateEnum {
+    First,
 }
