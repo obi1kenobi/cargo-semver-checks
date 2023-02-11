a31e1b90587fcf163a4ab2792c233a88689d895a
Said Aroua <said.aroua@bluewin.ch>
2023-02-11 20:57:18 +0100

    Add tests for deprecated unions
    Unions are not supported yet by the query engine,
    so the output file doesn't change.

--- test_crates/enum_struct_union_deprecated_added/old/src/lib.rs
+++ test_crates/enum_struct_union_deprecated_added/old/src/lib.rs
@@ -87,3 +87,48 @@ pub enum DeprecatedMessageEnumToDeprecatedMessageEnum {
 enum DeprecatedPrivateEnum {
     First,
 }
+
+// UNIONS
+
+// Adding #[deprecated] to these unions should be reported.
+
+pub union UnionToDeprecatedUnion {
+    bar: u64,
+}
+
+pub union UnionToDeprecatedMessageUnion {
+    bar: u64,
+}
+
+// These unions already have the attribute and changes to the attribute should not be reported.
+
+#[deprecated]
+pub union DeprecatedUnionToUnion {
+    bar: u64,
+}
+
+#[deprecated]
+pub union DeprecatedUnionToDeprecatedMessageUnion {
+    bar: u64,
+}
+
+#[deprecated = "This attribute will be deleted"]
+pub union DeprecatedMessageUnionToUnion {
+    bar: u64,
+}
+
+#[deprecated = "This message will be deleted"]
+pub union DeprecatedMessageUnionToDeprecatedUnion {
+    bar: u64,
+}
+
+#[deprecated = "This message will change"]
+pub union DeprecatedMessageUnionToDeprecatedMessageUnion {
+    bar: u64,
+}
+
+// This union is private and should NOT be reported.
+
+union DeprecatedPrivateUnion {
+    bar: u64,
+}
