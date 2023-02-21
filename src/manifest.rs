f11bb842472f34aec84120d4b570647ab7db9671
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-02-21 18:56:18 +0000

    Better error reporting when manifests fail to parse.

--- src/manifest.rs
+++ src/manifest.rs
@@ -11,36 +11,26 @@ impl Manifest {
         // Parsing via `cargo_toml::Manifest::from_path()` is preferable to parsing from a string,
         // because inspection of surrounding files is sometimes necessary to determine
         // the existence of lib targets and ensure proper handling of workspace inheritance.
-
         let parsed = cargo_toml::Manifest::from_path(&path)
-            .with_context(|| format!("Failed when reading {}", path.display()))?;
+            .with_context(|| format!("failed when reading {}", path.display()))?;
 
         Ok(Self { path, parsed })
     }
 }
 
 pub(crate) fn get_package_name(manifest: &Manifest) -> anyhow::Result<&str> {
-    let package = manifest.parsed.package.as_ref().ok_or_else(|| {
-        anyhow::format_err!(
-            "Failed to parse {}: no `package` table",
-            manifest.path.display()
-        )
+    let package = manifest.parsed.package.as_ref().with_context(|| {
+        format!("failed to parse {}: no `package` table", manifest.path.display())
     })?;
     Ok(&package.name)
 }
 
 pub(crate) fn get_package_version(manifest: &Manifest) -> anyhow::Result<&str> {
-    let package = manifest.parsed.package.as_ref().ok_or_else(|| {
-        anyhow::format_err!(
-            "Failed to parse {}: no `package` table",
-            manifest.path.display()
-        )
+    let package = manifest.parsed.package.as_ref().with_context(|| {
+        format!("failed to parse {}: no `package` table", manifest.path.display())
     })?;
     let version = package.version.get().with_context(|| {
-        format!(
-            "Failed to retrieve package version from {}",
-            manifest.path.display(),
-        )
+        format!("failed to retrieve package version from {}", manifest.path.display())
     })?;
     Ok(version)
 }
