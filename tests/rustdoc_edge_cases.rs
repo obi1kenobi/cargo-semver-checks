1dda3d4c73021c62ccbc64513bbbcf8a7f5f1b66
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2023-04-20 23:30:23 -0400

    Load lib target name from `cargo metadata`. (#433)
    * Load lib target name from `cargo metadata`.
    
    * Clean up test cases.
    
    * Reproduce the `perf-event-open-sys2` crash in CI.
    
    * Restore old behavior around bin targets.
    
    * Support proc macro crates too.

---
+++ tests/rustdoc_edge_cases.rs
@@ -0,0 +1,77 @@
+use assert_cmd::Command;
+
+/// This test checks whether the tool correctly detects
+/// implicit features defined by target-specific dependencies.
+/// https://github.com/obi1kenobi/cargo-semver-checks/issues/369
+#[test]
+fn detects_target_dependencies() {
+    let mut cmd = Command::cargo_bin("cargo-semver-checks").unwrap();
+    cmd.current_dir("test_crates/target_specific_dependencies")
+        .args(["semver-checks", "check-release", "--baseline-root=."])
+        .assert()
+        .success();
+}
+
+/// Ensure that crate and lib target names with dashes work properly.
+/// Rustdoc uses the lib target name with dashes replaced with underscores as the JSON file name.
+/// https://github.com/obi1kenobi/cargo-semver-checks/issues/432
+#[test]
+fn lib_target_with_dashes() {
+    let mut cmd = Command::cargo_bin("cargo-semver-checks").unwrap();
+    cmd.current_dir("test_crates/lib-target-with-dashes")
+        .args(["semver-checks", "check-release", "--baseline-root=."])
+        .assert()
+        .success();
+}
+
+/// Ensure that proc macro crates can be semver-checked correctly.
+#[test]
+fn proc_macro_target() {
+    let mut cmd = Command::cargo_bin("cargo-semver-checks").unwrap();
+    cmd.current_dir("test_crates/proc_macro_crate")
+        .args(["semver-checks", "check-release", "--baseline-root=."])
+        .assert()
+        .success();
+}
+
+/// Ensure that crates whose lib targets have a different name can be semver-checked correctly.
+/// Rustdoc uses the lib target name with dashes replaced with underscores as the JSON file name.
+/// https://github.com/obi1kenobi/cargo-semver-checks/issues/432
+#[test]
+fn renamed_lib_target() {
+    let mut cmd = Command::cargo_bin("cargo-semver-checks").unwrap();
+    cmd.current_dir("test_crates/renamed_lib_target")
+        .args(["semver-checks", "check-release", "--baseline-root=."])
+        .assert()
+        .success();
+}
+
+/// Ensure that semver-checking a crate that uses workspace-provided values works fine.
+#[test]
+fn crate_in_workspace() {
+    // Run at workspace level.
+    let mut cmd = Command::cargo_bin("cargo-semver-checks").unwrap();
+    cmd.current_dir("test_crates/crate_in_workspace")
+        .args([
+            "semver-checks",
+            "check-release",
+            "--manifest-path=./Cargo.toml",
+            "--baseline-root=.",
+        ])
+        .assert()
+        .success();
+
+    // Run at workspace level but point out the crate explicitly.
+    let mut cmd = Command::cargo_bin("cargo-semver-checks").unwrap();
+    cmd.current_dir("test_crates/crate_in_workspace")
+        .args([
+            "semver-checks",
+            "check-release",
+            "--manifest-path=./Cargo.toml",
+            "-p",
+            "crate1",
+            "--baseline-root=.",
+        ])
+        .assert()
+        .success();
+}
