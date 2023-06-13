adffa09ffe707c7d3245f6ef4f37fa6e5367e2ad
era <me@elias.sh>
2023-06-13 09:52:56 +0100

    add more testcases

--- test_crates/trait_removed_supertrait/new/src/lib.rs
+++ test_crates/trait_removed_supertrait/new/src/lib.rs
@@ -1,5 +1,17 @@
 pub trait SuperTrait {}
+pub trait SuperTrait2 {}
+pub trait GenericSuperTrait<T> {}
 
-pub trait BaseTrait {}
+pub trait RemovingSingleTrait {}
+pub trait RemovingOneTraitOfMultiple : SuperTrait  {}
+pub trait RemovingOneWithGenerics<T> : SuperTrait  {}
+pub trait RemovingOneWithGenericsOnTheSuperTrait<T> : SuperTrait  {}
+pub trait RemovingTraitAndLifetime<'a> : SuperTrait {}
+pub trait RemovingMultiple {}
 
 pub trait CorrectTrait : SuperTrait {}
+pub trait CorrectTraitMultipleSuperTraits : SuperTrait + SuperTrait2 {}
+pub trait CorrectTraitRemovingLifetime<'a> : SuperTrait {}
+pub trait CorrectTraitChangingTheGenericTypeBreaking : GenericSuperTrait<String> {}
+pub trait CorrectTraitChangingTheGenericTypeNonBreaking : GenericSuperTrait<i64> {}
+pub trait NotPresentOnPreviousVersion {}
