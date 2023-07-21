cbdccd687680d012924948df7db789eed0b596a2
U9G <git@u9g.dev>
2023-07-21 12:11:30 -0400

    Add some more tests

--- test_crates/trait_associated_type_removed/new/src/lib.rs
+++ test_crates/trait_associated_type_removed/new/src/lib.rs
@@ -1,4 +1,13 @@
-pub trait Foo {
-    type Apple;
-    fn throw() -> Self::Apple;
+#![feature(associated_type_defaults)]
+// removing a type from a trait is breaking.
+pub trait RemovedTypeFromTrait {}
+
+pub trait TraitWithType {
+    type MyType;
 }
+
+// removing a default from a type in a trait is breaking, but for a different lint.
+pub trait RemovedTypeDefaultFromTrait: TraitWithType {}
+
+// this trait is private therefore removing a type change is not breaking.
+trait PrivateTraitWithTypeRemoved {}
--- test_crates/trait_associated_type_removed/old/src/lib.rs
+++ test_crates/trait_associated_type_removed/old/src/lib.rs
@@ -1,5 +1,24 @@
-pub trait Foo {
+#![feature(associated_type_defaults)]
+pub trait RemovedTypeFromTrait {
     type Apple;
-    type Bar;
-    fn throw() -> Self::Apple;
+}
+
+pub trait TraitWithType {
+    type MyType;
+}
+
+pub trait RemovedTypeDefaultFromTrait: TraitWithType {
+    type MyType = u8;
+}
+
+// this trait is private therefore removing a type change is not breaking.
+trait PrivateTraitWithTypeRemoved {
+    type MyType;
+}
+
+/// This trait should not be reported by the `trait_associated_type_removed` rule:
+/// it will be removed altogether, so the correct rule to catch it is
+/// the `trait_missing` rule and not the rule for missing types.
+trait RemovedTrait {
+    type ATypeInARemovedTrait;
 }
