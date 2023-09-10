c853f31cecbadf72bb065ad289577e6b6da220e2
Nick Mathewson <nickm@torproject.org>
2023-09-09 21:14:22 -0400

    fixup! Add a new "module_missing" lint
    Adjust formatting on test_crates/module_missing

--- test_crates/module_missing/old/src/lib.rs
+++ test_crates/module_missing/old/src/lib.rs
@@ -1,17 +1,13 @@
-mod a {
-}
+mod a {}
 
 mod b {
-    pub mod b {
-    }
+    pub mod b {}
 }
 
 pub mod bb {
-    pub mod will_remove {
-    }
+    pub mod will_remove {}
 }
 
 pub mod will_make_private {
-    mod e {
-    }
+    mod e {}
 }
