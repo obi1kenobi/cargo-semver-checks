97956ce5f62a749f5976ee11e259a8bca0128f7f
samuelrayment <samrayment@gmail.com>
2023-09-25 17:33:04 +0100

    New lint for removed associated constant in trait (#539)
    * Adds lint for removed associated constant from trait
    
    * Make span `@optional` and format query.
    
    We want to make the span `@optional` because that information is only intended to help us emit a better diagnostic message. If for any reason it isn't present, we still want to flag the issue even without span data.
    
    ---------
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>

---
+++ test_crates/trait_removed_associated_constant/new/src/lib.rs
@@ -0,0 +1,11 @@
+pub trait RemovedAssociatedConstantFromTrait {
+}
+
+pub trait TraitWithConstant {
+    const APPLE: i32;
+}
+
+pub trait RemovedAssociatedConstantDefaultFromTrait: TraitWithConstant {}
+
+// this trait is private therefore removing the constant is not breaking
+trait PrivateTraitWithTypeRemoved {}
