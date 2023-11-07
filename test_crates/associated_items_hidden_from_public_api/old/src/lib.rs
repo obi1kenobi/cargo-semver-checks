94ed3ebf1a618f9c31b35db0ea2d362c15f73bfe
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-11-07 20:53:19 +0000

    Add test cases for module contents and associated items being hidden.

---
+++ test_crates/associated_items_hidden_from_public_api/old/src/lib.rs
@@ -0,0 +1,16 @@
+pub struct Example;
+
+impl Example {
+    #[doc(hidden)]
+    pub fn directly_hidden_fn() {}
+
+    #[doc(hidden)]
+    pub const DIRECTLY_HIDDEN_CONST: i64 = 42;
+}
+
+#[doc(hidden)]
+impl Example {
+    pub fn impl_hidden_fn() {}
+
+    pub const IMPL_HIDDEN_CONST: i64 = 42;
+}
---
+++ test_crates/module_contents_hidden_from_public_api/new/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod removals {}
+
+#[doc(hidden)]
+pub fn changed_fn_signature(x: i64, y: i64, z: i64) {
+    assert_eq!(x + y, z);
+}
