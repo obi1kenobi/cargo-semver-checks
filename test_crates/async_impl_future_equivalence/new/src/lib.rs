61102eeed7bb27c3d96113386b45fb830b89b371
Jimmy Wu <jimmywu013@gmail.com>
2024-01-06 12:55:07 -0500

    Add test crate to test async fn and impl Future

---
+++ test_crates/async_impl_future_equivalence/new/src/lib.rs
@@ -0,0 +1,27 @@
+//! async fn and return position impl Future are equivalent.
+//! Switching from one to the other should NOT generate any lints
+
+use std::future::Future;
+
+pub fn switches_to_return_impl() -> impl Future {
+    async {}
+}
+
+pub async fn switches_to_async() {}
+
+pub struct S;
+
+impl S {
+    pub fn switches_to_return_impl() -> impl Future {
+        async {}
+    }
+
+    pub async fn switches_to_async() {}
+}
+
+#[allow(async_fn_in_trait)]
+pub trait Trait {
+    fn switches_to_return_impl() -> impl Future;
+
+    async fn switches_to_async();
+}
---
+++ test_crates/async_impl_future_equivalence/old/src/lib.rs
@@ -0,0 +1,27 @@
+//! async fn and return position impl Future are equivalent.
+//! Switching from one to the other should NOT generate any lints
+
+use std::future::Future;
+
+pub async fn switches_to_return_impl() {}
+
+pub fn switches_to_async() -> impl Future {
+    async {}
+}
+
+pub struct S;
+
+impl S {
+    pub async fn switches_to_return_impl() {}
+
+    pub fn switches_to_async() -> impl Future {
+        async {}
+    }
+}
+
+#[allow(async_fn_in_trait)]
+pub trait Trait {
+    async fn switches_to_return_impl();
+
+    fn switches_to_async() -> impl Future;
+}
