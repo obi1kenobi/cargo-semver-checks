d8b4ffe5e099decfca01d2f74099cc3fbcb0e225
Predrag Gruevski <obi1kenobi82@gmail.com>
2024-02-13 14:45:59 +0000

    Remove ability to check bin-only targets, to fix `doc = false` issue.
    Because of a complex web of edge cases, we can _either_ choose to continue to support our legacy behavior of allowing bin-only targets to be semver-checked, _or_ we can support semver-checking lib targets that set `doc = false`. Semver-checking bin-only targets doesn't make a lot of sense, since they cannot be used as a library anyway. This functionality was always slated for removal, I just wish we had more time to deprecate & warn on it before just ripping it out like this.
    
    Now we're in a tight spot, and this is definitely the lesser evil -- we're making the tool work for library users who produce an API that can reasonably be checked and used as such.
    
    Fixes #660.

--- tests/rustdoc_edge_cases.rs
+++ tests/rustdoc_edge_cases.rs
@@ -37,6 +37,19 @@ fn proc_macro_target() {
         .failure();
 }
 
+/// Ensure that crates with only a bin target (so, no lib target) produce the correct error message
+/// since they have no library API and therefore nothing we can semver-check.
+#[test]
+fn bin_target_only() {
+    let mut cmd = Command::cargo_bin("cargo-semver-checks").unwrap();
+    cmd.current_dir("test_crates/bin_target_only")
+        .args(["semver-checks", "check-release", "--baseline-root=."])
+        .env_remove("RUST_BACKTRACE")
+        .assert()
+        .stderr("error: no crates with library targets selected, nothing to semver-check\n")
+        .failure();
+}
+
 /// Ensure that crates whose lib target has `crate-type = ["rlib"]`
 /// can be semver-checked correctly.
 #[test]
