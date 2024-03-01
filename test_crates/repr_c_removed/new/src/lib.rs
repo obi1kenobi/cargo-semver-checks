f5d9deea875603bae3d16d4d2fd1c83931331cc8
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-03-01 09:20:49 +0000

    Add union to repr_c_removed lint

--- test_crates/repr_c_removed/new/src/lib.rs
+++ test_crates/repr_c_removed/new/src/lib.rs
@@ -82,3 +82,41 @@ pub enum SeparateCU8ToSeparateU8CEnum {
     Bar,
     Baz,
 }
+
+pub struct CStructToStruct {
+    pub bar: usize,
+}
+
+#[repr(align(16))]
+pub struct Align16CStructToAlign16Struct {
+    pub bar: usize,
+}
+
+#[repr(align(16))]
+pub struct SeparateAlign16CStructToAlign16Struct {
+    pub bar: usize,
+}
+
+#[repr(align(16))]
+pub struct CAlign16StructToAlign16Struct {
+    pub bar: usize,
+}
+
+pub union CUnionToUnion {
+    pub bar: usize,
+}
+
+#[repr(align(16))]
+pub union Align16CUnionToAlign16Union {
+    pub bar: usize,
+}
+
+#[repr(align(16))]
+pub union SeparateAlign16CUnionToAlign16Union {
+    pub bar: usize,
+}
+
+#[repr(align(16))]
+pub union CAlign16UnionToAlign16Union {
+    pub bar: usize,
+}
