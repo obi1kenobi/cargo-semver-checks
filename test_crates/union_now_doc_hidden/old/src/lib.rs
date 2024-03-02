89c5a662e409aa27851606e40dcc79df73c25f63
vasu <vasucp1207@gmail.com>
2024-03-02 05:31:34 +0530

    file renaming

--- test_crates/union_now_doc_hidden/old/src/lib.rs
+++ test_crates/union_now_doc_hidden/old/src/lib.rs
@@ -2,3 +2,12 @@ pub union MyUnion {
     f1: u32,
     f2: f32,
 }
+
+pub union PublicUnionHiddenField {
+    pub my_field: i8,
+}
+
+pub union PublicUnionDocumentedWithStringHidden {
+    f1: u32,
+    f2: f32,
+}
