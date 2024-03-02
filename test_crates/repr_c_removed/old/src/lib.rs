491bf138415547b69aa3d46154c612797d45abbe
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-03-02 01:16:35 +0000

    Replace per-type `repr(C)` lints with a single lint for all of struct/enum/union (#678)
    * Add union to repr_c_removed lint
    
    * Lowercase the owner type in the error message template
    
    ---------
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>

--- test_crates/repr_c_removed/old/src/lib.rs
+++ test_crates/repr_c_removed/old/src/lib.rs
@@ -29,7 +29,6 @@ pub enum SeparateCU8EnumToU8Enum {
     Baz,
 }
 
-
 // The following enums have *rearrangements* of repr(C), potentially
 // splitting singular repr(*) into multiple, smaller repr(*) or merging
 // repr(*) into larger repr(*).
@@ -92,4 +91,46 @@ pub enum U8CToCU8Enum {
 pub enum SeparateCU8ToSeparateU8CEnum {
     Bar,
     Baz,
+}
+
+#[repr(C)]
+pub struct CStructToStruct {
+    pub bar: usize,
+}
+
+#[repr(align(16), C)]
+pub struct Align16CStructToAlign16Struct {
+    pub bar: usize,
+}
+
+#[repr(align(16))]
+#[repr(C)]
+pub struct SeparateAlign16CStructToAlign16Struct {
+    pub bar: usize,
+}
+
+#[repr(C, align(16))]
+pub struct CAlign16StructToAlign16Struct {
+    pub bar: usize,
+}
+
+#[repr(C)]
+pub union CUnionToUnion {
+    pub bar: usize,
+}
+
+#[repr(align(16), C)]
+pub union Align16CUnionToAlign16Union {
+    pub bar: usize,
+}
+
+#[repr(align(16))]
+#[repr(C)]
+pub union SeparateAlign16CUnionToAlign16Union {
+    pub bar: usize,
+}
+
+#[repr(C, align(16))]
+pub union CAlign16UnionToAlign16Union {
+    pub bar: usize,
 }
