e3716346d601c46c883b8ac7ff88533bad5e53f5
Arpit Yadav <128481255+arpity22@users.noreply.github.com>
2024-03-09 00:52:17 +0530

    Added lint enum_struct_variant_field_now_doc_hidden (#687)
    * Add lint enum_tuple_variant_field_now_hidden
    
    * modifications
    
    * Added lint enum_tuple_variant_field_now_doc_hidden
    
    * Added format modifications
    
    * Added lint enum_struct_variant_field_now_doc_hidden
    
    * corrections
    
    * Update src/lints/enum_struct_variant_field_now_doc_hidden.ron
    
    ---------
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>

---
+++ test_crates/enum_struct_variant_field_now_doc_hidden/new/src/lib.rs
@@ -0,0 +1,33 @@
+// should not be caught since field was already marked #[doc(hidden)]
+pub enum PublicEnum {
+    StructVariant {
+        #[doc(hidden)]
+        x: i64,
+        y: u8,
+    },
+}
+//Basic Test case should be caught
+pub enum PublicEnumA {
+    StructVariant {
+        #[doc(hidden)]
+        x: i64,
+        y: u8,
+    },
+}
+// Non Public Enum should not be affected on adding #[doc(hidden)]
+enum NonPublicEnum {
+    StructVariant {
+        #[doc(hidden)]
+        x: i32,
+        y: u8,
+    },
+}
+//should not be caught since enum was not a part of public API
+#[doc(hidden)]
+pub enum PublicEnumB {
+    StructVariant {
+        #[doc(hidden)]
+        x: i32,
+        y: i64,
+    },
+}
---
+++ test_crates/enum_struct_variant_field_now_doc_hidden/old/src/lib.rs
@@ -0,0 +1,17 @@
+pub enum PublicEnum {
+    StructVariant {
+        #[doc(hidden)]
+        x: i64,
+        y: u8,
+    },
+}
+pub enum PublicEnumA {
+    StructVariant { x: i64, y: u8 },
+}
+enum NonPublicEnum {
+    StructVariant { x: i32, y: u8 },
+}
+#[doc(hidden)]
+pub enum PublicEnumB {
+    StructVariant { x: i32, y: i64 },
+}
