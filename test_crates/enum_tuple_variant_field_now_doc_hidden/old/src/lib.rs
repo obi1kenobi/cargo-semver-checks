cb7e8b264efe66e6417c8d9dcb83b906addf242d
arpity22 <arpity22@iitk.ac.in>
2024-03-08 21:17:19 +0530

    Added format modifications

--- test_crates/enum_tuple_variant_field_now_doc_hidden/old/src/lib.rs
+++ test_crates/enum_tuple_variant_field_now_doc_hidden/old/src/lib.rs
@@ -1,13 +1,13 @@
 enum NonPublicEnum {
-    TupleVariant(i32,u8),
+    TupleVariant(i32, u8),
 }
 pub enum PulicEnumA {
-    TupleVariantWithPublicField(u8,i32),
+    TupleVariantWithPublicField(u8, i32),
 }
 pub enum PulicEnumB {
     TupleVariantWithPublicFieldHidden(#[doc(hidden)] i64, u8),
 }
 #[doc(hidden)]
-pub enum PulicEnumC{
-    TupleVariantWithPublicField(u8,i64),
+pub enum PulicEnumC {
+    TupleVariantWithPublicField(u8, i64),
 }
