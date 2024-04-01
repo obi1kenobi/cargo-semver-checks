8bcd6811ad39e3e35a068e148cd957dfb97817ca
jw013 <jw013@users.noreply.github.com>
2024-03-31 23:44:32 -0400

    Add union_missing lint (#731)

--- test_crates/union_field_missing/new/src/lib.rs
+++ test_crates/union_field_missing/new/src/lib.rs
@@ -3,20 +3,24 @@ union PrivateUnion {
     f3: f32,
 }
 
-
 // pub union with private fields shouldn't cause any breaking changes
 pub union PubUnionPrivateField {
     f3: f32,
 }
 
-
 // pub union with pub fields renamed should cause breaking changes
-pub union PubUnionPubFieldRenamed{
+pub union PubUnionPubFieldRenamed {
     pub f1: u32,
     pub f3: f32,
 }
 
 // pub union with pub fields removed should cause breaking changes
-pub union PubUnionPubFieldRemoved{
+pub union PubUnionPubFieldRemoved {
     pub f1: u32,
+}
+
+// this should only trigger the union_missing lint, not union_field_missing
+union PubUnionBecomesPrivateAndPubFieldRemoved {
+    pub f1: u32,
+    f2: f32,
 }
