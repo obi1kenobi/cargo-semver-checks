264c67c8820c083943dd9bed8e003a21d1dbc77b
Predrag Gruevski <obi1kenobi82@gmail.com>
2024-04-18 21:12:38 +0000

    Impl item removals should lint if the fallback item is not public API.

--- test_crates/inherent_method_missing/old/src/lib.rs
+++ test_crates/inherent_method_missing/old/src/lib.rs
@@ -12,3 +12,42 @@ impl Foo {
 
     pub fn moved_method(&self) {}
 }
+
+// Test that the removal of an inherent method `next()` does not trigger `inherent_method_missing`
+// if the equivalent method `Iterator::next()` is available and public API.
+pub struct MyIter;
+
+impl MyIter {
+    // This method is getting removed.
+    pub fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        <Self as Iterator>::next()
+    }
+}
+
+impl Iterator for MyIter {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
+// Test that the removal of an inherent method `next()` triggers `inherent_method_missing`
+// if there is an equivalent method `Iterator::next()` that isn't public API via `#[doc(hidden)]`.
+pub struct SecretlyIter;
+
+impl SecretlyIter {
+    // This method is getting removed.
+    pub fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        <Self as Iterator>::next()
+    }
+}
+
+#[doc(hidden)]
+impl Iterator for SecretlyIter {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
