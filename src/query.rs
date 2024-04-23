ee091f46fd4491c4cac9e37d5029a7ad49bf54c3
Arpit Yadav <128481255+arpity22@users.noreply.github.com>
2024-04-23 22:54:57 +0530

    Add lint pub_static_mut_now_immutable (#768)
    * Add lint pub_static_mut_now_immutable
    
    * Modifications in error message and description
    
    * Modifications in description and error message
    
    * Modifications in description and error message
    
    * Modifications in description and error message

--- src/query.rs
+++ src/query.rs
@@ -516,6 +516,7 @@ add_lints!(
     pub_module_level_const_missing,
     pub_module_level_const_now_doc_hidden,
     pub_static_missing,
+    pub_static_mut_now_immutable,
     pub_static_now_doc_hidden,
     repr_c_removed,
     repr_packed_added,
