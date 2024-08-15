77324c6420892e979778a684a1bc6e719f8f4bcd
Pedro Turik Firmino <pedroturik@gmail.com>
2024-08-15 16:06:14 -0300

    adds urlencoding as a dependency and generates github issue URLs based on sysinfo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use std::{env, path::PathBuf};
 
+use anstyle::{Color, Reset, Style};
 use cargo_semver_checks::{
     GlobalConfig, PackageSelection, ReleaseType, Rustdoc, ScopeSelection, SemverQuery,
 };
@@ -15,14 +16,7 @@ fn main() {
     configure_color(args.color_choice);
 
     if args.bugreport {
-        use bugreport::{bugreport, collector::*, format::Markdown};
-        bugreport!()
-            .info(SoftwareVersion::default())
-            .info(OperatingSystem::default())
-            .info(CommandLine::default())
-            .info(CommandOutput::new("cargo version", "cargo", &["-V"]))
-            .info(CompileTimeInformation::default())
-            .print::<Markdown>();
+        print_issue_url();
         std::process::exit(0);
     } else if args.list {
         exit_on_error(true, || {
@@ -141,6 +135,42 @@ fn configure_color(cli_choice: Option<clap::ColorChoice>) {
     };
 
     choice.write_global();
+}
+
+fn print_issue_url() {
+    use bugreport::{bugreport, collector::*, format::Markdown};
+    let false_negative_url: &str = "https://github.com/obi1kenobi/cargo-semver-checks/issues/new?labels=A-lint%2CC-enhancement&template=2-false-negative.yml";
+    let false_positive_url: &str = "https://github.com/obi1kenobi/cargo-semver-checks/issues/new?labels=C-bug%2CA-lint&template=1-false-positive.yml";
+    let other_bug: &str = "https://github.com/obi1kenobi/cargo-semver-checks/issues/new?labels=C-bug&template=3-bug-report.yml";
+
+    let bug_report = bugreport!()
+        .info(SoftwareVersion::default())
+        .info(OperatingSystem::default())
+        .info(CommandLine::default())
+        .info(CommandOutput::new("cargo version", "cargo", &["-V"]))
+        .info(CompileTimeInformation::default())
+        .format::<Markdown>();
+
+    let bug_report_url: String = urlencoding::encode(&bug_report).into_owned();
+
+    println!(
+        "{}",
+        format!(
+            "Please file a bug report at one of the following URLs:\n\n\
+            {}- False negative:{} {}sysinfo={bug_report_url}\n\
+            {}- False positive:{} {}sysinfo={bug_report_url}\n\
+            {}- Other bug:{} {}sysinfo={bug_report_url}\n\n",
+            Style::new().fg_color(Some(Color::Ansi(anstyle::AnsiColor::Cyan))),
+            Reset,
+            false_negative_url,
+            Style::new().fg_color(Some(Color::Ansi(anstyle::AnsiColor::Cyan))),
+            Reset,
+            false_positive_url,
+            Style::new().fg_color(Some(Color::Ansi(anstyle::AnsiColor::Cyan))),
+            Reset,
+            other_bug
+        )
+    );
 }
 
 #[derive(Debug, Parser)]
