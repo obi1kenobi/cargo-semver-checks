62a3c75215399f9c5d853b7dad24147af6a01f42
Max Carr <m@mcarr.one>
2024-08-16 10:42:58 -0700

    Add tests for `publish = false` workspace members explicitly included with `--package` (#866)
    * add redactions for total checks and filenames
    
    * filter only the root of the repo.
    
    * add tests for `--package` flag on `publish = false` workspace members
    
    * use filtered snapshots
    
    * update snapshot
    
    * use `--verbose` flag to show that it is skipping packages
    
    * Apply suggestions from code review
    
    * Update src/snapshot_tests.rs
    
    ---------
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>

--- src/snapshot_tests.rs
+++ src/snapshot_tests.rs
@@ -243,3 +243,50 @@ fn workspace_all_publish_false() {
         ],
     );
 }
+
+/// Running `cargo semver-checks` on a workspace with a `publish = false` member,
+/// explicitly including that member with `--package`.  Currently, this executes
+/// `cargo semver-checks`.
+#[test]
+fn workspace_publish_false_explicit() {
+    assert_integration_test(
+        "workspace_publish_false_explicit",
+        &[
+            "cargo",
+            "semver-checks",
+            "--manifest-path",
+            "test_crates/manifest_tests/workspace_all_publish_false/new",
+            "--baseline-root",
+            "test_crates/manifest_tests/workspace_all_publish_false/old",
+            "--package",
+            "a",
+        ],
+    )
+}
+
+/// Running `cargo semver-checks` on a workspace with a `package = false` member,
+/// explicitly including that member with `--package` and also specifying `--workspace`.
+/// Currently, `--workspace` overrides `--package` and the `publish = false` member is not
+/// semver-checked, and `cargo-semver-checks` silently exits 0.
+///
+/// Changing this behavior to make it more consistent is tracked in:
+/// https://github.com/obi1kenobi/cargo-semver-checks/issues/868
+#[test]
+fn workspace_publish_false_workspace_flag() {
+    assert_integration_test(
+        "workspace_publish_false_workspace_flag",
+        &[
+            "cargo",
+            "semver-checks",
+            "--manifest-path",
+            "test_crates/manifest_tests/workspace_all_publish_false/new",
+            "--baseline-root",
+            "test_crates/manifest_tests/workspace_all_publish_false/old",
+            "--workspace",
+            "--package",
+            "a",
+            // use verbose mode to show what is being skipped
+            "--verbose",
+        ],
+    )
+}
