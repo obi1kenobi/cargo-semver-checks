6e968c4306f8b2daa80a4f9e585efe85b6c5a505
Milo Moisson <milomoisson@gmail.com>
2024-08-23 20:10:55 +0200

    tests might prove useful ;)

--- test_crates/trait_associated_const_added/new/src/lib.rs
+++ test_crates/trait_associated_const_added/new/src/lib.rs
@@ -1,3 +1,30 @@
+mod private {
+    pub(crate) trait Sealed {}
+}
+
 pub trait Foo {
     const BAR: bool;
+}
+
+pub trait FooSealed: private::Sealed {
+    const BAR: bool;
+}
+pub trait FooNotSealed: private::Sealed {
+    const BAR: bool;
+}
+
+pub trait Bar {
+    const BAR: bool = true;
+}
+pub trait BarSealed: private::Sealed {
+    const BAR: bool = true;
+}
+
+pub trait Baz {
+    const ONE: bool;
+    const TWO: bool;
+}
+pub trait BazSealed: private::Sealed {
+    const ONE: bool;
+    const TWO: bool;
 }
--- test_crates/trait_associated_const_added/old/src/lib.rs
+++ test_crates/trait_associated_const_added/old/src/lib.rs
@@ -1,1 +1,18 @@
+mod private {
+    pub(crate) trait Sealed {}
+}
+
 pub trait Foo {}
+
+pub trait FooSealed: private::Sealed {}
+pub trait FooNotSealed {}
+
+pub trait Bar {}
+pub trait BarSealed: private::Sealed {}
+
+pub trait Baz {
+    const ONE: bool;
+}
+pub trait BazSealed: private::Sealed {
+    const ONE: bool;
+}
