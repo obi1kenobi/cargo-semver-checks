d35782a25dc365a5481ec864079d1d9913517e31
David Matos <davidmatos06@gmail.com>
2024-08-25 23:43:00 +0200

    Add non-object-safe test case
    Add test cases for ensuring edge cases around sealed/non sealed
    
    minor typos

--- test_crates/trait_default_impl_removed/new/src/lib.rs
+++ test_crates/trait_default_impl_removed/new/src/lib.rs
@@ -1,3 +1,8 @@
+mod private {
+    pub trait Sealed {}
+    pub struct Token;
+}
+
 // Default trait method impl is removed, should be reported
 pub trait TraitA {
     fn method_default_impl_removed(&self);
@@ -7,12 +12,29 @@ pub trait TraitA {
 // missing instead
 pub trait TraitB {}
 
-//Default trait method impl is removed, but its private, shouldnt be reported
+// Default trait method impl is removed, but it's private, shouldn't be reported
 trait TraitC {
     fn method_default_impl_removed(&self);
 }
 
-// Non-object safe trait has its default impl removed, should be reported
+// Default trait method impl is removed, and becomes non-object-safe, should be reported
 pub trait TraitD {
-    fn method_becomes_non_obj_safe(&self) -> Self;
+    fn method_default_impl_removed_and_becomes_non_obj_safe(&self) -> Self;
+}
+
+// Default trait method impl is removed, and becomes sealed, should be reported
+pub trait TraitE {
+    fn method_default_impl_removed_and_becomes_sealed(&self, _: private::Token);
+}
+
+// Default trait method impl is removed, and it's partially sealed, should be reported
+pub trait TraitF {
+    fn method_partially_sealed_has_default_impl_removed(&self, _: private::Token);
+}
+
+// Default trait method impl is removed, but it's sealed. The fact that it's sealed dominates,
+// thus having its default impl removed is not a problem since it was never possible to
+// implement it, should not be reported
+pub trait TraitG: private::Sealed {
+    fn method_sealed_has_default_impl_removed(&self);
 }
