8af671cc32f162057af4a595eefbb7b2b419ba90
Milo Moisson <milomoisson@gmail.com>
2024-08-27 10:06:56 +0200

    feat: add trait_associated_type_default_removed lint

---
+++ test_crates/trait_associated_type_default_removed/new/src/lib.rs
@@ -0,0 +1,25 @@
+mod sealed {
+    pub(crate) trait Sealed {}
+}
+
+pub trait WillLoseDefault {
+    type Foo;
+}
+
+pub trait WillLoseDefaultSealed: sealed::Sealed {
+    type Foo;
+}
+
+pub trait Unchanged {
+    type Foo = bool;
+}
+pub trait UnchangedSealed: sealed::Sealed {
+    type Foo = bool;
+}
+
+pub trait UnchangedNoDefault {
+    type Foo;
+}
+pub trait UnchangedNoDefaultSealed: sealed::Sealed {
+    type Foo;
+}
---
+++ test_crates/trait_associated_type_default_removed/old/src/lib.rs
@@ -0,0 +1,25 @@
+mod sealed {
+    pub(crate) trait Sealed {}
+}
+
+pub trait WillLoseDefault {
+    type Foo = bool;
+}
+
+pub trait WillLoseDefaultSealed: sealed::Sealed {
+    type Foo = bool;
+}
+
+pub trait Unchanged {
+    type Foo = bool;
+}
+pub trait UnchangedSealed: sealed::Sealed {
+    type Foo = bool;
+}
+
+pub trait UnchangedNoDefault {
+    type Foo;
+}
+pub trait UnchangedNoDefaultSealed: sealed::Sealed {
+    type Foo;
+}
