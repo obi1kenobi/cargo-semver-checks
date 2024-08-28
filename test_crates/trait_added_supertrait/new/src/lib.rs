46df55909e065435caaa6932a688eacc06bdbfbe
Milo Moisson <milomoisson@gmail.com>
2024-08-28 23:15:01 +0200

    typo and std supertraits

--- test_crates/trait_added_supertrait/new/src/lib.rs
+++ test_crates/trait_added_supertrait/new/src/lib.rs
@@ -1,15 +1,21 @@
+pub trait TraitOne {}
+pub trait TraitTwo {}
+
 mod sealed {
     pub trait Sealed {}
 }
 
-pub trait TraitOne {}
-pub trait TraitTwo {}
-
 pub trait Unchanged {}
 pub trait UnchangedSealed: sealed::Sealed {}
 
-pub trait WillGailOne: TraitOne {}
-pub trait WillGailOneSealed: TraitOne + sealed::Sealed {}
+pub trait WillGainOne: TraitOne {}
+pub trait WillGainOneSealed: TraitOne + sealed::Sealed {}
 
-pub trait WillGailAnotherOne: TraitOne + TraitTwo {}
-pub trait WillGailAnotherOneSealed: TraitOne + TraitTwo + sealed::Sealed {}
+pub trait WillGainAnotherOne: TraitOne + TraitTwo {}
+pub trait WillGainAnotherOneSealed: TraitOne + TraitTwo + sealed::Sealed {}
+
+pub trait WillGainStdOne: Sync {}
+pub trait WillGainStdTwo: core::fmt::Debug {}
+pub trait WillGainStdThree: PartialEq {
+    fn make_me_non_object_safe() -> Self;
+}
--- test_crates/trait_added_supertrait/old/src/lib.rs
+++ test_crates/trait_added_supertrait/old/src/lib.rs
@@ -1,15 +1,21 @@
+pub trait TraitOne {}
+pub trait TraitTwo {}
+
 mod sealed {
     pub trait Sealed {}
 }
 
-pub trait TraitOne {}
-pub trait TraitTwo {}
-
 pub trait Unchanged {}
 pub trait UnchangedSealed: sealed::Sealed {}
 
-pub trait WillGailOne {}
-pub trait WillGailOneSealed: sealed::Sealed {}
+pub trait WillGainOne {}
+pub trait WillGainOneSealed: sealed::Sealed {}
 
-pub trait WillGailAnotherOne: TraitOne {}
-pub trait WillGailAnotherOneSealed: TraitOne + sealed::Sealed {}
+pub trait WillGainAnotherOne: TraitOne {}
+pub trait WillGainAnotherOneSealed: TraitOne + sealed::Sealed {}
+
+pub trait WillGainStdOne {}
+pub trait WillGainStdTwo {}
+pub trait WillGainStdThree {
+    fn make_me_non_object_safe() -> Self;
+}
