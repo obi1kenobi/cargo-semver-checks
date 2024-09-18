907454ee37eb9f5da372ddc2b0bd2f12251cb315
David Matos <davidmatos06@gmail.com>
2024-09-19 00:05:37 +0200

    Add two test cases to cover `non_exhaustive`
    Typos and grammar

--- test_crates/enum_no_repr_variant_discriminant_changed/new/src/lib.rs
+++ test_crates/enum_no_repr_variant_discriminant_changed/new/src/lib.rs
@@ -1,5 +1,5 @@
 // Explicit discriminant changed values. By doing so, it changed the implicit
-// discriminant's value as well, should be reported.
+// discriminant's value as well. Should be reported.
 pub enum ExplicitAndImplicitDiscriminantsAreChanged {
     First = 2,
     Second,
@@ -7,15 +7,15 @@ pub enum ExplicitAndImplicitDiscriminantsAreChanged {
 }
 
 // Discriminant changed values. Having #[non_exhaustive] on the enum should not have any effect
-// on the *API* breakage, should still be reported.
+// on the *API* breakage. Should be reported.
 #[non_exhaustive]
 pub enum DiscriminantIsChanged {
     First = 1,
 }
 
 // Discriminant changed values and the variant is also marked as `non_exhaustive`.
-// This now implies that the enum is no longer `well-defined`, which means that a numeric
-// cast is no longer possible on the enum, should not be reported.
+// This now implies that the discriminant is no longer `well-defined`, which means that a numeric
+// cast is no longer possible on the enum. Should not be reported.
 // https://github.com/rust-lang/reference/pull/1249#issuecomment-2339003824
 #[non_exhaustive]
 pub enum DiscriminantIsChangedAndMarkedNonExhaustive {
@@ -25,8 +25,8 @@ pub enum DiscriminantIsChangedAndMarkedNonExhaustive {
 }
 
 // Discriminant changed values, but the variant is already `non_exhaustive`.
-// This means that the enum is already not `well-defined`, and the numeric cast
-// was never possible, should not be reported.
+// This means that the discriminant is already not `well-defined`, and the numeric cast
+// was never possible. Should not be reported.
 #[non_exhaustive]
 pub enum DiscriminantIsChangedButAlreadyNonExhaustive {
     First,
@@ -34,23 +34,23 @@ pub enum DiscriminantIsChangedButAlreadyNonExhaustive {
     Second = 2,
 }
 
-// Discriminant changed to be doc hidden and explicit. Being doc hidden is not relevant
-// since it's still part of the public API, should be reported.
+// Variant became doc(hidden) while variant became explicit. Being doc(hidden) is not relevant
+// since it's still part of the public API. Should be reported.
 pub enum DiscriminantBecomesDocHiddenAndExplicit {
     First,
     #[doc(hidden)]
     Second = 2,
 }
 
-// Explicit discriminants changed values, but being private dominates, should not be
+// Explicit discriminants changed values, but being private dominates. Should not be
 // reported.
 enum PrivateEnum {
     First = 10,
     Second = 11,
 }
 
-// Discriminant changed value, but with repr, should not be reported as it involves ABI
-// breakage as well, and this is linted at *API* only breakage.
+// Discriminant changed values, but the enum has a `repr` attr. Should not be reported as it
+// involves ABI breakage as well, and this is linted at *API* only breakage.
 #[repr(u8)]
 pub enum FieldlessWithDiscrimants {
     First = 10,
@@ -60,7 +60,7 @@ pub enum FieldlessWithDiscrimants {
     Last = 21,
 }
 
-// Implicit discriminant value changed by becoming explicit, should not be reported as it involves
+// Implicit discriminant value changed by becoming explicit. Should not be reported as it involves
 // ABI breakage as well, and this is linted at *API* only breakage.
 #[repr(u8)]
 pub enum FieldlessChangesToExplicitDescriminant {
@@ -69,10 +69,27 @@ pub enum FieldlessChangesToExplicitDescriminant {
     Unit = 5,
 }
 
-// Variant `Struct` acquires a field. By doing so, it makes the enum not `well-defined`,
-// meaning it is not possible to do a numeric cast anymore on the enum, should not be reported.
+// Variant `Struct` acquires a field. By doing so, it makes the discriminant not `well-defined`,
+// meaning it is not possible to do a numeric cast anymore on the enum. Should not be reported.
 pub enum UnitOnlyBecomesUndefined {
     First,
     Second,
     Struct { a: i64 },
+}
+
+// Discriminant changed values, but the other variant is marked `non_exhaustive`.
+// This means that a numeric cast is not possible on the enum. Should not be reported.
+#[non_exhaustive]
+pub enum DiscriminantIsChangedButAVariantIsNonExhaustive {
+    #[non_exhaustive]
+    First,
+    Second = 2,
+}
+
+// Discriminant changed values, and the variant is no longer `non_exhaustive`.
+// The fact that the variant is no longer `non_exhaustive` is not relevant, since a numeric cast
+// was impossible to begin with. Should not be reported.
+pub enum DiscriminantIsChangedAndVariantNoLongerNonExhaustive {
+    First,
+    Second = 2,
 }
--- test_crates/enum_no_repr_variant_discriminant_changed/old/src/lib.rs
+++ test_crates/enum_no_repr_variant_discriminant_changed/old/src/lib.rs
@@ -1,5 +1,5 @@
 // Explicit discriminant changed values. By doing so, it changed the implicit
-// discriminant's value as well, should be reported.
+// discriminant's value as well. Should be reported.
 pub enum ExplicitAndImplicitDiscriminantsAreChanged {
     First = 1,
     Second,
@@ -7,15 +7,15 @@ pub enum ExplicitAndImplicitDiscriminantsAreChanged {
 }
 
 // Discriminant changed values. Having #[non_exhaustive] on the enum should not have any effect
-// on the *API* breakage, should still be reported.
+// on the *API* breakage. Should be reported.
 #[non_exhaustive]
 pub enum DiscriminantIsChanged {
     First,
 }
 
 // Discriminant changed values and the variant is also marked as `non_exhaustive`.
-// This now implies that the enum is no longer `well-defined`, which means that a numeric
-// cast is no longer possible on the enum, should not be reported.
+// This now implies that the discriminant is no longer `well-defined`, which means that a numeric
+// cast is no longer possible on the enum. Should not be reported.
 // https://github.com/rust-lang/reference/pull/1249#issuecomment-2339003824
 #[non_exhaustive]
 pub enum DiscriminantIsChangedAndMarkedNonExhaustive {
@@ -24,8 +24,8 @@ pub enum DiscriminantIsChangedAndMarkedNonExhaustive {
 }
 
 // Discriminant changed values, but the variant is already `non_exhaustive`.
-// This means that the enum is already not `well-defined`, and the numeric cast
-// was never possible, should not be reported.
+// This means that the discriminant is already not `well-defined`, and the numeric cast
+// was never possible. Should not be reported.
 #[non_exhaustive]
 pub enum DiscriminantIsChangedButAlreadyNonExhaustive {
     First,
@@ -33,22 +33,22 @@ pub enum DiscriminantIsChangedButAlreadyNonExhaustive {
     Second,
 }
 
-// Discriminant changed to be doc hidden and explicit. Being doc hidden is not relevant
-// since it's still part of the public API, should be reported.
+// Variant became doc(hidden) while variant became explicit. Being doc(hidden) is not relevant
+// since it's still part of the public API. Should be reported.
 pub enum DiscriminantBecomesDocHiddenAndExplicit {
     First,
     Second,
 }
 
-// Explicit discriminants changed values, but being private dominates, should not be
+// Explicit discriminants changed values, but being private dominates. Should not be
 // reported.
 enum PrivateEnum {
     First = 1,
     Second = 2,
 }
 
-// Discriminant changed value, but with repr, should not be reported as it involves ABI
-// breakage as well, and this is linted at *API* only breakage.
+// Discriminant changed values, but the enum has a `repr` attr. Should not be reported as it
+// involves ABI breakage as well, and this is linted at *API* only breakage.
 #[repr(u8)]
 pub enum FieldlessWithDiscrimants {
     First = 10,
@@ -58,7 +58,7 @@ pub enum FieldlessWithDiscrimants {
     Last = 20,
 }
 
-// Implicit discriminant value changed by becoming explicit, should not be reported as it involves
+// Implicit discriminant value changed by becoming explicit. Should not be reported as it involves
 // ABI breakage as well, and this is linted at *API* only breakage.
 pub enum FieldlessChangesToExplicitDescriminant {
     Tuple(),
@@ -66,10 +66,28 @@ pub enum FieldlessChangesToExplicitDescriminant {
     Unit,
 }
 
-// Variant `Struct` acquires a field. By doing so, it makes the enum not `well-defined`,
-// meaning it is not possible to do a numeric cast anymore on the enum, should not be reported.
+// Variant `Struct` acquires a field. By doing so, it makes the discriminant not `well-defined`,
+// meaning it is not possible to do a numeric cast anymore on the enum. Should not be reported.
 pub enum UnitOnlyBecomesUndefined {
     First,
     Second,
     Struct {},
+}
+
+// Discriminant changed values, but the other variant is marked `non_exhaustive`.
+// This means that a numeric cast is not possible on the enum. Should not be reported.
+#[non_exhaustive]
+pub enum DiscriminantIsChangedButAVariantIsNonExhaustive {
+    #[non_exhaustive]
+    First,
+    Second,
+}
+
+// Discriminant changed values, and the variant is no longer `non_exhaustive`.
+// The fact that the variant is no longer `non_exhaustive` is not relevant, since a numeric cast
+// was impossible to begin with. Should not be reported.
+pub enum DiscriminantIsChangedAndVariantNoLongerNonExhaustive {
+    #[non_exhaustive]
+    First,
+    Second,
 }
