a5b526767419c61b18669ed9d16ac9ea8c90e4ae
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2024-10-17 22:20:10 -0400

    Apply suggestions from code review

--- test_crates/enum_variant_missing/old/src/lib.rs
+++ test_crates/enum_variant_missing/old/src/lib.rs
@@ -13,7 +13,7 @@ pub enum TupleVariantWillBeRemoved {
 pub enum StructVariantWillBeRemoved {
     Foo { bar: usize },
     /// Testing: <https://doc.rust-lang.org/cargo/reference/semver.html#item-remove>
-    Bar{ bar: usize },
+    Bar { bar: usize },
 }
 
 /// This enum should not be reported by the `enum_variant_missing` rule:
