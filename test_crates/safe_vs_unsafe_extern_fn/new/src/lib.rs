9474431a5c72188f6f0bd7a82f5665f3980463c9
Predrag Gruevski <obi1kenobi82@gmail.com>
2024-11-27 02:50:37 +0000

    Test that externally-defined functions going safe -> unsafe are caught.

---
+++ test_crates/safe_vs_unsafe_extern_fn/new/src/lib.rs
@@ -0,0 +1,27 @@
+unsafe extern "C" {
+    pub fn originally_legacy_now_unsafe_extern_and_implicit_unsafe();
+
+    pub unsafe fn originally_legacy_now_unsafe_extern_and_explicit_unsafe();
+
+    pub safe fn originally_legacy_now_unsafe_extern_and_safe();
+}
+
+unsafe extern "C" {
+    pub fn originally_explicit_now_implicit_unsafe();
+
+    pub unsafe fn originally_implicit_now_explicit_unsafe();
+
+    pub fn originally_safe_now_implicit_unsafe();
+
+    pub unsafe fn originally_safe_now_explicit_unsafe();
+}
+
+// This is the "legacy" way of specifying `extern`.
+extern "C" {
+    pub fn originally_implicit_unsafe_now_legacy();
+
+    pub fn originally_explicit_unsafe_now_legacy();
+
+    // This is a breaking change!
+    pub fn originally_safe_now_legacy();
+}
---
+++ test_crates/safe_vs_unsafe_extern_fn/old/src/lib.rs
@@ -0,0 +1,25 @@
+extern "C" {
+    pub fn originally_legacy_now_unsafe_extern_and_implicit_unsafe();
+
+    pub fn originally_legacy_now_unsafe_extern_and_explicit_unsafe();
+
+    pub fn originally_legacy_now_unsafe_extern_and_safe();
+}
+
+unsafe extern "C" {
+    pub unsafe fn originally_explicit_now_implicit_unsafe();
+
+    pub fn originally_implicit_now_explicit_unsafe();
+
+    pub safe fn originally_safe_now_implicit_unsafe();
+
+    pub safe fn originally_safe_now_explicit_unsafe();
+}
+
+unsafe extern "C" {
+    pub fn originally_implicit_unsafe_now_legacy();
+
+    pub unsafe fn originally_explicit_unsafe_now_legacy();
+
+    pub safe fn originally_safe_now_legacy();
+}
