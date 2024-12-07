965d62aadec125662060a70ec970c721fda5d13e
Malena Gruevski <45406876+malenaohl@users.noreply.github.com>
2024-12-07 17:57:05 -0500

    Add lint attribute_proc_macro_missing (#1026)
    Addresses a checkbox in
    https://github.com/obi1kenobi/cargo-semver-checks/issues/946

---
+++ test_crates/attribute_proc_macro_missing/old/src/lib.rs
@@ -0,0 +1,18 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn logging(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+// Keep this to verify we don't trigger on function-like macros
+#[proc_macro]
+pub fn sql(_item: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
+
+// Keep this to verify we don't trigger on derive macros
+#[proc_macro_derive(MyDerive)]
+pub fn my_derive(_item: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
