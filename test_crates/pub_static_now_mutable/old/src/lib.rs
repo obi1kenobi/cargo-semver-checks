a7e8c891c5403ad9e8db3a79dd3dced1a8c214a7
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-12-07 21:40:19 +0300

    feat: add pub_static_now_mutable lint (#1020)
    This new lint is for public static variables which might be mutable
    later on. This would require them to use an unsafe block which is
    breaking.

---
+++ test_crates/pub_static_now_mutable/old/src/lib.rs
@@ -0,0 +1,25 @@
+// Basic Test cases
+pub static STATIC_A: i32 = 0;
+pub static mut STATIC_B: i32 = 0;
+static STATIC_C: i32 = 0;
+
+// Test case for #[doc(hidden)] pub static
+#[doc(hidden)]
+pub static DOC_HIDDEN_STATIC_A: i32 = 0;
+
+// Renaming or making a static #[doc(hidden)] along with making it mutable
+// should trigger only one lint
+#[doc(hidden)]
+pub static DOC_HIDDEN_STATIC_B: i32 = 0;
+pub static STATIC_RENAMED: i32 = 0;
+
+// Testing for static defined in private module
+mod PRIVATE_MODULE {
+    pub static STATIC_C: i32 = 0;
+}
+
+// Testing for static defined in #[doc(hidden)] module
+#[doc(hidden)]
+pub mod DOC_HIDDEN_MODULE {
+    pub static STATIC_C: i32 = 0;
+}
