a9fe57d92c623e4331f8706fb634b3c9f76205d0
Predrag Gruevski <obi1kenobi82@gmail.com>
2024-12-20 19:08:12 +0000

    Add the `type_requires_more_const_generic_params` lint.
    It checks for structs/enums/unions that begin requiring a higher minimum number of const generic arguments than they used to.

---
+++ test_crates/type_requires_more_generic_consts/old/src/lib.rs
@@ -0,0 +1,17 @@
+#[non_exhaustive]
+pub struct NotGeneric {}
+
+#[non_exhaustive]
+pub enum DefaultBecomesRequired<const N: usize = 1, const M: usize = 1> {
+    First([i64; N]),
+    Second([i64; M]),
+}
+
+pub union ConstGenericAdded<T> {
+    left: std::mem::ManuallyDrop<[T; 16]>,
+}
+
+// This one isn't breaking, so it shouldn't be flagged!
+pub union DefaultedConstGenericAdded<T> {
+    left: std::mem::ManuallyDrop<[T; 16]>,
+}
