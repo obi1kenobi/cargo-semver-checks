13d9c5895b7b5f9f759b9f25ff1066896429861c
Frank Wang <1454884738@qq.com>
2025-01-27 14:41:20 -0600

    rename all `object_safe` to `dyn_compatible`

--- test_crates/trait_associated_const_added/new/src/lib.rs
+++ test_crates/trait_associated_const_added/new/src/lib.rs
@@ -5,59 +5,59 @@ mod sealed {
 pub trait WillGainConstWithoutDefault {
     const BAR: bool;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 
 pub trait WillGainConstWithoutDefaultSealed: sealed::Sealed {
     const BAR: bool;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 pub trait WillGainConstWithoutDefaultAndSeal: sealed::Sealed {
     const BAR: bool;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 
 pub trait WillGainConstWithDefault {
     const BAR: bool = true;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 pub trait WillGainConstWithDefaultSealed: sealed::Sealed {
     const BAR: bool = true;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 
 pub trait WillGainAnotherConstWithoutDefault {
     const ONE: bool;
     const TWO: bool;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 pub trait WillGainAnotherConstWithoutDefaultSealed: sealed::Sealed {
     const ONE: bool;
     const TWO: bool;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 
 pub trait WillGainADocHiddenConst {
     #[doc(hidden)]
     const BAR: bool;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 
 pub trait ConstWithoutDefaultUnchanged {
     const BAR: bool;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
 pub trait ConstDocHidden {
     #[doc(hidden)]
     const BAR: bool = true;
 
-    fn make_me_non_object_safe() -> Self;
+    fn make_me_non_dyn_compatible() -> Self;
 }
