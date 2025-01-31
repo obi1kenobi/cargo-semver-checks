51aefbd0b9070730814141fd8bd990c26445ab0e
Frank Wang <1454884738@qq.com>
2025-01-31 15:22:20 -0600

    new lint: `function_marked_deprecated`

---
+++ test_crates/function_marked_deprecated/new/src/lib.rs
@@ -0,0 +1,49 @@
+// These functions did not have the #[deprecated] attribute in the old version.
+// Addition of the attribute should be reported by this rule.
+
+#[deprecated]
+pub fn function_to_deprecated() -> i32 {
+    42
+}
+
+#[deprecated = "This attribute was added"]
+pub fn function_to_deprecated_message(input: &str) -> String {
+    input.to_string()
+}
+
+// These functions had the #[deprecated] attribute in the old version. Changes of
+// the attribute, including deletion, should NOT be reported by this rule.
+
+pub fn deprecated_function_to_function(data: &[u8]) -> bool {
+    true
+}
+
+#[deprecated = "This message was added"]
+pub fn deprecated_function_to_deprecated_message() -> Option<i32> {
+    None
+}
+
+// These functions had the #[deprecated] attribute in the old version.
+// They also included the user-defined warning message. Changes of
+// the attribute, including deletion, should NOT be reported by this rule.
+
+pub fn deprecated_message_function_to_function() {
+    // Empty implementation
+}
+
+#[deprecated]
+pub fn deprecated_message_function_to_deprecated() {
+    // Empty implementation
+}
+
+#[deprecated = "This message was changed"]
+pub fn deprecated_message_function_to_deprecated_message() {
+    // Empty implementation
+}
+
+// This function is private and should NOT be reported by this rule.
+
+#[deprecated]
+fn deprecated_private_function() {
+    // Empty implementation
+}
---
+++ test_crates/function_marked_deprecated/old/src/lib.rs
@@ -0,0 +1,48 @@
+// These functions did not have the #[deprecated] attribute in the old version.
+// Addition of the attribute should be reported by this rule.
+
+pub fn function_to_deprecated() -> i32 {
+    42
+}
+
+pub fn function_to_deprecated_message(input: &str) -> String {
+    input.to_string()
+}
+
+// These functions had the #[deprecated] attribute in the old version. Changes of
+// the attribute, including deletion, should NOT be reported by this rule.
+
+#[deprecated]
+pub fn deprecated_function_to_function(data: &[u8]) -> bool {
+    true
+}
+
+#[deprecated]
+pub fn deprecated_function_to_deprecated_message() -> Option<i32> {
+    None
+}
+
+// These functions had the #[deprecated] attribute in the old version.
+// They also included the user-defined warning message. Changes of
+// the attribute, including deletion, should NOT be reported by this rule.
+
+#[deprecated = "This attribute will be deleted"]
+pub fn deprecated_message_function_to_function() {
+    // Empty implementation
+}
+
+#[deprecated = "This message will be deleted"]
+pub fn deprecated_message_function_to_deprecated() {
+    // Empty implementation
+}
+
+#[deprecated = "This message will change"]
+pub fn deprecated_message_function_to_deprecated_message() {
+    // Empty implementation
+}
+
+// This function is private and should NOT be reported by this rule.
+
+fn deprecated_private_function() {
+    // Empty implementation
+}
