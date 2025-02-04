9840ea642e3764a9154746703f61045459e5a254
Frank Wang <1454884738@qq.com>
2025-02-04 15:24:54 -0600

    new lint: `macro_marked_deprecated`

--- src/query.rs
+++ src/query.rs
@@ -1182,6 +1182,7 @@ add_lints!(
     inherent_method_must_use_added,
     inherent_method_now_doc_hidden,
     inherent_method_unsafe_added,
+    macro_marked_deprecated,
     macro_no_longer_exported,
     macro_now_doc_hidden,
     method_parameter_count_changed,
---
+++ test_crates/macro_marked_deprecated/new/src/lib.rs
@@ -0,0 +1,79 @@
+// These macros are now deprecated and should be reported
+#[deprecated]
+#[macro_export]
+macro_rules! macro_to_deprecated {
+    () => {
+        42
+    };
+}
+
+#[deprecated = "This macro is deprecated"]
+#[macro_export]
+macro_rules! macro_to_deprecated_message {
+    ($x:expr) => {
+        $x + 1
+    };
+}
+
+// These macros should not trigger the lint
+#[macro_export]
+macro_rules! macro_stays_normal {
+    ($x:expr) => {
+        $x.to_string()
+    };
+}
+
+#[deprecated]
+#[macro_export]
+macro_rules! macro_already_deprecated {
+    () => {
+        true
+    };
+}
+
+#[deprecated = "New message"]
+#[macro_export]
+macro_rules! macro_message_changes {
+    () => {
+        "hello"
+    };
+}
+
+// Private macros should not be reported
+#[deprecated]
+macro_rules! private_macro_to_deprecated {
+    () => {
+        0
+    };
+}
+
+// Hidden macros should not be reported
+#[doc(hidden)]
+#[deprecated]
+#[macro_export]
+macro_rules! hidden_macro_to_deprecated {
+    () => {
+        1
+    };
+}
+
+mod foo {
+    // Public macro. Exported even though it's in a private module,
+    // because of the `#[macro_export]`.
+    #[deprecated]
+    #[macro_export]
+    macro_rules! inner_macro_exported_to_deprecated {
+        () => {
+            100
+        };
+    }
+}
+
+mod bar {
+    #[deprecated]
+    macro_rules! inner_macro_to_deprecated {
+        () => {
+            100
+        };
+    }
+}
