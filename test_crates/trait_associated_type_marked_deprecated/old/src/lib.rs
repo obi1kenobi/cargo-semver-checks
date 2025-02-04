99c6f1867c847a6c2f74b040024b02a3344ceb49
Frank Wang <1454884738@qq.com>
2025-02-04 10:22:23 -0600

    new lint `trait_associated_type_marked_deprecated`

---
+++ test_crates/trait_associated_type_marked_deprecated/old/src/lib.rs
@@ -0,0 +1,39 @@
+pub trait TraitWithTypeToBeDeprecated {
+    // This associated type will be marked deprecated
+    type TypeToDeprecated: Clone;
+
+    // This associated type will be marked deprecated with a message
+    type TypeToDeprecatedMessage: ToString;
+
+    // These types should not trigger the lint
+    type TypeStaysNormal;
+
+    #[deprecated]
+    type TypeAlreadyDeprecated;
+
+    #[deprecated = "old message"]
+    type TypeMessageChanges;
+}
+
+// Changes to private trait types should not be reported
+trait PrivateTrait {
+    type PrivateTypeToDeprecated;
+}
+
+#[deprecated]
+pub trait DeprecatedTrait {
+    // Adding deprecated to a type in an already deprecated trait should not be reported
+    type TypeInDeprecatedTrait;
+}
+
+// Hidden trait - changes to its types should not be reported
+#[doc(hidden)]
+pub trait HiddenTrait {
+    type TypeBecomesDeprecated;
+}
+
+// Public trait with hidden type should not be reported
+pub trait PublicTraitWithHiddenType {
+    #[doc(hidden)]
+    type HiddenTypeBecomesDeprecated;
+}
