356dfad82ffa8f263e55dcb4b0ba8c0160eb9fc2
frankwang <73262844+Frank-III@users.noreply.github.com>
2025-02-06 08:42:36 -0800

    new lint: `type_method_marked_deprecated` (#1130)
    part of: #57

---
+++ test_crates/type_method_marked_deprecated/old/src/structs.rs
@@ -0,0 +1,82 @@
+pub struct NormalStruct {
+    value: i32,
+}
+
+impl NormalStruct {
+    pub fn method_to_be_deprecated(&self) -> i32 {
+        self.value
+    }
+
+    pub fn method_with_message_to_be_deprecated(&self) -> String {
+        "hello".to_string()
+    }
+
+    // Should not trigger - already deprecated
+    #[deprecated]
+    pub fn already_deprecated_method(&self) {}
+
+    // Should not trigger - stays normal
+    pub fn stays_normal(&self) {}
+}
+
+#[doc(hidden)]
+pub struct HiddenStruct {
+    value: i32,
+}
+
+impl HiddenStruct {
+    // Should not trigger - type is hidden
+    pub fn method_to_be_deprecated(&self) {}
+}
+
+pub struct StructWithHiddenImpl {
+    value: i32,
+}
+
+#[doc(hidden)]
+impl StructWithHiddenImpl {
+    // Should not trigger - impl is hidden
+    pub fn method_to_be_deprecated(&self) {}
+}
+
+pub struct StructWithHiddenMethod {
+    value: i32,
+}
+
+impl StructWithHiddenMethod {
+    #[doc(hidden)]
+    pub fn method_to_be_deprecated(&self) {}
+}
+
+// Private struct - should not trigger
+struct PrivateStruct {
+    value: i32,
+}
+
+impl PrivateStruct {
+    pub fn method_to_be_deprecated(&self) {}
+}
+
+// This struct will become deprecated, but methods stay normal
+pub struct StructBecomesDeprecated {
+    value: i32,
+}
+
+impl StructBecomesDeprecated {
+    pub fn method_stays_normal(&self) -> i32 {
+        self.value
+    }
+
+    pub fn another_normal_method(&self) -> String {
+        "hello".to_string()
+    }
+}
+
+// Both struct and methods are deprecated
+pub struct BothStructAndMethodDeprecated {
+    value: i32,
+}
+
+impl BothStructAndMethodDeprecated {
+    pub fn method_to_be_deprecated(&self) {}
+}
