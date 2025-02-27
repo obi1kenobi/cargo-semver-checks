3fd4dc56ee73f6d341957249d48d7d34ee254a18
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-02-27 10:29:57 +0530

    feat: more examples on test_crates

--- test_crates/trait_sealed_becomes_unsealed/old/src/lib.rs
+++ test_crates/trait_sealed_becomes_unsealed/old/src/lib.rs
@@ -16,3 +16,27 @@ pub mod hidden_module {
 /// This trait is public-API-sealed since implementing it requires naming
 /// its non-public-API supertrait.
 pub trait HiddenSealedInherited: hidden_module::HiddenSealed {}
+
+/// This trait is public-API-sealed because its method's return type is doc-hidden,
+/// so external implementers would have to name a non-public-API type to write the impl.
+pub trait MethodReturnHiddenSealed {
+    fn method(&self) -> hidden_module::Token;
+}
+
+/// This trait is public-API-sealed transitively because of its supertrait.
+pub trait TransitivelyHiddenSealed: HiddenSealedInherited {}
+
+/// This trait is public-API-sealed, since `Self: hidden_module::HiddenSealed`
+/// still requires that `Self` implement a public-API-sealed trait,
+/// even though the public-API-sealed trait isn't *exactly* a supertrait.
+pub trait HiddenSealedWithWhereSelfBound
+where
+    Self: hidden_module::HiddenSealed,
+{
+}
+
+/// This trait is public-API-sealed because its method's argument type is doc-hidden,
+/// so external implementers would have to name a non-public-API type to write the impl.
+pub trait MethodHiddenSealed {
+    fn method(&self, token: hidden_module::Token);
+}
