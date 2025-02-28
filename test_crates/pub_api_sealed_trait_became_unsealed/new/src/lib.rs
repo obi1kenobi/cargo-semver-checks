5426f017093d7095ae8b50aabbbf7aeef954a373
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-02-28 14:56:39 +0530

    fix: lint detecting unconditionally_sealed -> unsealed traits

--- test_crates/pub_api_sealed_trait_became_unsealed/new/src/lib.rs
+++ test_crates/pub_api_sealed_trait_became_unsealed/new/src/lib.rs
@@ -1,48 +1,39 @@
-pub mod public_api_hidden_module_to_be_exposed {
-    pub trait Sealed {}
-    pub struct Token;
-}
-
-mod public_api_hidden_module_to_be_unconditionally_hidden {
-    pub trait Sealed {}
-    pub struct Token;
-}
-
-// ─────────────────────────────────────────────────────
 // Traits transitioning from Public API Sealed → Unsealed (Lint should detect these)
-// ─────────────────────────────────────────────────────
+pub mod public_api_sealed_to_unsealed {
+    pub mod hidden {
+        pub trait Sealed {}
+        pub struct Token;
+    }
 
-// Start: Public API Sealed → Unsealed (Lint should detect these)
-pub trait PublicAPIToBeUnsealed {
-    type Hidden;
+    pub trait PublicAPIToBeUnsealed {
+        type Hidden;
+    }
+
+    pub trait TraitExtendsHiddenPublicAPITrait: hidden::Sealed {}
+
+    pub trait MethodReturnPublicAPIHiddenToken {
+        fn method(&self) -> hidden::Token;
+    }
+
+    pub trait MethodTakingPublicAPIHiddenToken {
+        fn method(&self, token: hidden::Token);
+    }
 }
 
-pub trait TraitExtendsPublicAPIHiddenTrait: public_api_hidden_module_to_be_exposed::Sealed {}
-
-pub trait MethodReturnPublicAPIHiddenToken {
-    fn method(&self) -> public_api_hidden_module_to_be_exposed::Token;
-}
-
-pub trait MethodTakingPublicAPIHiddenToken {
-    fn method(&self, token: public_api_hidden_module_to_be_exposed::Token);
-}
-// End: Public API Sealed → Unsealed
-
-// ─────────────────────────────────────────────────────
 // Traits transitioning from Public API Sealed → Unconditionally Sealed (Lint should ignore these)
-// ─────────────────────────────────────────────────────
+pub mod public_api_sealed_to_unconditionally_sealed {
+    mod hidden {
+        pub trait Sealed {}
+        pub struct Token;
+    }
 
-// Start: Public API Sealed → Unconditionally Sealed (Lint should ignore these)
-pub trait TraitExtendsUnconditionallyHiddenTrait:
-    public_api_hidden_module_to_be_unconditionally_hidden::Sealed
-{
-}
+    pub trait TraitExtendsUnconditionallyHiddenTrait: hidden::Sealed {}
 
-pub trait MethodReturningUnconditionallyHiddenToken {
-    fn method(&self) -> public_api_hidden_module_to_be_unconditionally_hidden::Token;
-}
+    pub trait MethodReturningUnconditionallyHiddenToken {
+        fn method(&self) -> hidden::Token;
+    }
 
-pub trait MethodTakingUnconditionallyHiddenToken {
-    fn method(&self, token: public_api_hidden_module_to_be_unconditionally_hidden::Token);
+    pub trait MethodTakingUnconditionallyHiddenToken {
+        fn method(&self, token: hidden::Token);
+    }
 }
-// End: Public API Sealed → Unconditionally Sealed
