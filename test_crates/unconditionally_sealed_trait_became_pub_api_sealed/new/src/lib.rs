802e06f61cf907d4241173466b5dd9aa68db6880
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-03-02 21:15:22 +0530

    new lint: `unconditionally_sealed_trait_became_pub_api_sealed` (#1167)
    Resolves #1123

---
+++ test_crates/unconditionally_sealed_trait_became_pub_api_sealed/new/src/lib.rs
@@ -0,0 +1,24 @@
+// Traits transitioning from Unconditionally Sealed -> Public API Sealed (Lint should detect these)
+pub mod unconditionally_sealed_to_public_api_sealed {
+    #[doc(hidden)]
+    pub mod hidden {
+        pub trait Sealed {}
+        pub struct Token;
+    }
+
+    pub trait ExtendsTraitInHiddenModule: hidden::Sealed {}
+
+    pub trait ReturnsTraitInHiddenModule {
+        fn method(&self) -> hidden::Token;
+    }
+
+    pub trait AcceptsTraitInHiddenModule {
+        fn method(&self, token: hidden::Token);
+    }
+
+    pub trait SealedWithWhereSelfBound
+    where
+        Self: hidden::Sealed,
+    {
+    }
+}
