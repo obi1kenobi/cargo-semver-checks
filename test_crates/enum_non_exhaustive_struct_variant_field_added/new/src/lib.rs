3614e7e043055d511b277e1df0f20cc47f75c7e5
shreyans <shreyans431@gmail.com>
2025-03-14 00:25:14 +0530

    Add enum_non_exhaustive_struct_variant_field_added

---
+++ test_crates/enum_non_exhaustive_struct_variant_field_added/new/src/lib.rs
@@ -0,0 +1,28 @@
+#![no_std]
+
+pub enum PubEnum {
+    #[non_exhaustive]
+    Foo {
+        x: i64,
+        y: usize,
+    }
+}
+
+// This enum is not public, so it should not be reported by the lint,
+// regardless of the new field in the variant.
+pub(crate) enum PrivateEnum {
+    #[non_exhaustive]
+    Foo {
+        x: i64,
+        y: usize,
+    }
+}
+
+// This enum's variant will become exhaustive while also gaining a field.
+// That should trigger a separate lint, and not this one.
+pub enum EnumWithNonExhaustiveVariant {
+    NonExhaustiveVariant {
+        x: i64,
+        y: usize,
+    }
+}
