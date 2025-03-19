6908f4805db4b8e45c49ac5229eb712d152df379
shreyans <shreyans431@gmail.com>
2025-03-19 19:26:54 +0530

    Add enum_struct_variant_changed_kind lint.

---
+++ test_crates/enum_struct_variant_changed_kind/old/src/lib.rs
@@ -0,0 +1,43 @@
+#![no_std]
+
+// ---- Should be reported ----
+pub enum TestTuple {
+    WillBecomeTupleLike { field: i32 }
+}
+
+pub enum TestUnit {
+    WillBecomeUnitLike { field: i32 }
+}
+
+pub enum TestMultipleFields {
+    WillBecomeTupleLike { a: i32, b: f64 }
+}
+
+pub enum TestBecomeDocHidden {
+    WillBecomeTupleLike { field: i32 }
+}
+
+pub enum TestBecomeNonExhaustive {
+    WillBecomeTupleLike { field: i32 }
+}
+
+// ---- Should not be reported ----
+pub enum TestEmptyStruct {
+    EmptyStruct {}
+}
+
+pub enum TestTupleNonExhaustive {
+    #[non_exhaustive]
+    WillBecomeTupleLike { field: i32 }
+}
+
+pub enum TestDocHidden {
+    #[doc(hidden)]
+    WillBecomeTupleLike { field: i32 }
+}
+
+pub enum MultipleStayTheSame {
+    StructLike { field: i32 },
+    TupleLike(i32),
+    UnitLike,
+}
