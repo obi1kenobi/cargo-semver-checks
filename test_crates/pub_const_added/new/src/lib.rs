7c72cd3d4dbc3801051a2f93537d548a8ccf6063
shreyans <shreyans431@gmail.com>
2025-03-22 20:16:33 +0530

    Modified test cases

--- test_crates/pub_const_added/new/src/lib.rs
+++ test_crates/pub_const_added/new/src/lib.rs
@@ -1,20 +1,20 @@
 #![no_std]
 
 // This should not trigger this lint.
-pub const ExistingConst: i32 = 0;
+pub const EXISTING_CONST_IN_GLOBAL: i32 = 0;
 
 // This should not trigger this lint.
 pub const PUB_STATIC_IN_GLOBAL_WILL_BE_CONST: i32 = 0;
 
 // private const item added, this should not trigger this lint.
-const PrivateConst: i32 = 0;
+const PRIVATE_CONST_IN_GLOBAL: i32 = 0;
 
 // static item added, this should not trigger this lint.
-pub static NewStatic: i32 = 0;
+pub static NEW_STATIC_IN_GLOBAL: i32 = 0;
 
-pub const NewConst: i32 = 0;
+pub const NEW_CONST_IN_GLOBAL: i32 = 0;
 
 pub mod my_module {
-    pub const ExistingConstInModule: i32 = 0;
-    pub const NewConstInModule: i32 = 0; 
+    pub const EXISTING_CONST_IN_MODULE: i32 = 0;
+    pub const NEW_CONST_IN_MODULE: i32 = 0; 
 }
