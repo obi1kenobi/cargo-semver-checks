729c556ce2fa067d3b9726fdff5ed06b310f26a6
shreyans <shreyans431@gmail.com>
2025-03-22 20:25:21 +0530

    minor changes

--- test_crates/pub_const_added/new/src/lib.rs
+++ test_crates/pub_const_added/new/src/lib.rs
@@ -7,14 +7,14 @@ pub const EXISTING_CONST_IN_GLOBAL: i32 = 0;
 pub const PUB_STATIC_IN_GLOBAL_WILL_BE_CONST: i32 = 0;
 
 // private const item added, this should not trigger this lint.
-const PRIVATE_CONST_IN_GLOBAL: i32 = 0;
+const NEW_PRIVATE_CONST_IN_GLOBAL: i32 = 0;
 
 // static item added, this should not trigger this lint.
-pub static NEW_STATIC_IN_GLOBAL: i32 = 0;
+pub static NEW_PUB_STATIC_IN_GLOBAL: i32 = 0;
 
-pub const NEW_CONST_IN_GLOBAL: i32 = 0;
+pub const NEW_PUB_CONST_IN_GLOBAL: i32 = 0;
 
 pub mod my_module {
     pub const EXISTING_CONST_IN_MODULE: i32 = 0;
-    pub const NEW_CONST_IN_MODULE: i32 = 0; 
+    pub const NEW_PUB_CONST_IN_MODULE: i32 = 0; 
 }
