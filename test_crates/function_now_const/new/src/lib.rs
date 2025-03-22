ac1b75a5f51b473d6171c8a405a09899c1901241
shreyans <shreyans431@gmail.com>
2025-03-22 05:34:37 +0530

    Improved description, error message for the lint and added test cases.

--- test_crates/function_now_const/new/src/lib.rs
+++ test_crates/function_now_const/new/src/lib.rs
@@ -4,6 +4,17 @@ pub const fn add(x: i64, y: i64) -> i64 {
     x + y
 }
 
+// This fn is private and should not trigger the lint.
+const fn double(x: i32) -> i32 {
+    x * 2
+}
+
+// This fn is hidden and should not trigger the lint.
+#[doc(hidden)]
+pub const fn hidden_fn(x: i32) -> i32 {
+    x * 2
+}
+
 // New constant function added
 // This should not trigger the lint
 pub const fn new_const_fn(x: i64) -> i64 {
