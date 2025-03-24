e1f41fad781997836f2a5d0f28d8633a35ffe93c
shreyans <shreyans431@gmail.com>
2025-03-24 06:31:28 +0530

    Modified the lint.

--- test_crates/enum_non_exhaustive_struct_variant_field_added/new/src/lib.rs
+++ test_crates/enum_non_exhaustive_struct_variant_field_added/new/src/lib.rs
@@ -1,28 +1,25 @@
 #![no_std]
 
-pub enum PubEnum {
+pub enum ExhaustiveEnum {
     #[non_exhaustive]
-    Foo {
-        x: i64,
-        y: usize,
-    }
+    Foo { x: i64, y: usize },
+}
+
+#[non_exhaustive]
+pub enum NonExhaustiveEnum {
+    #[non_exhaustive]
+    Foo { x: i64, y: usize },
 }
 
 // This enum is not public, so it should not be reported by the lint,
 // regardless of the new field in the variant.
 pub(crate) enum PrivateEnum {
     #[non_exhaustive]
-    Foo {
-        x: i64,
-        y: usize,
-    }
+    Foo { x: i64, y: usize },
 }
 
 // This enum's variant will become exhaustive while also gaining a field.
 // That should trigger a separate lint, and not this one.
 pub enum EnumWithNonExhaustiveVariant {
-    NonExhaustiveVariant {
-        x: i64,
-        y: usize,
-    }
+    NonExhaustiveVariant { x: i64, y: usize },
 }
--- test_crates/enum_non_exhaustive_struct_variant_field_added/old/src/lib.rs
+++ test_crates/enum_non_exhaustive_struct_variant_field_added/old/src/lib.rs
@@ -1,26 +1,26 @@
 #![no_std]
 
-pub enum PubEnum {
+pub enum ExhaustiveEnum {
     #[non_exhaustive]
-    Foo {
-        x: i64,
-    }
+    Foo { x: i64 },
+}
+
+#[non_exhaustive]
+pub enum NonExhaustiveEnum {
+    #[non_exhaustive]
+    Foo { x: i64 },
 }
 
 // This enum is not public, so it should not be reported by the lint,
 // regardless of the new field in the variant.
 pub(crate) enum PrivateEnum {
     #[non_exhaustive]
-    Foo {
-        x: i64,
-    }
+    Foo { x: i64 },
 }
 
 // This enum's variant will become exhaustive while also gaining a field.
 // That should trigger a separate lint, and not this one.
 pub enum EnumWithNonExhaustiveVariant {
     #[non_exhaustive]
-    NonExhaustiveVariant {
-        x: i64,
-    }
+    NonExhaustiveVariant { x: i64 },
 }
