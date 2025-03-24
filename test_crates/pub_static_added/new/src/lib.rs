4079c7d36cc21b271de291372d804733400be9d0
shreyans <shreyans431@gmail.com>
2025-03-24 19:09:15 +0530

    Improved description, error message, test cases.

--- test_crates/pub_static_added/new/src/lib.rs
+++ test_crates/pub_static_added/new/src/lib.rs
@@ -1,20 +1,20 @@
 #![no_std]
 
 // This should not trigger this lint.
-pub static ExistingStatic: i32 = 0;
+pub static EXISTING_PUB_STATIC_IN_GLOBAL: i32 = 0;
 
 // This should not trigger this lint.
 pub static PUB_CONST_IN_GLOBAL_WILL_BE_STATIC: i32 = 0;
 
 // private static item added, this should not trigger this lint.
-static PrivateStatic: i32 = 0;
+static NEW_PRIVATE_STATIC_IN_GLOBAL: i32 = 0;
 
 // const item added, this should not trigger this lint.
-pub const NewConst: i32 = 0;
+pub const NEW_PUB_CONST_IN_GLOBAL: i32 = 0;
 
-pub static NewStatic: i32 = 0;
+pub static NEW_PUB_STATIC_IN_GLOBAL: i32 = 0;
 
 pub mod my_module {
-    pub static ExistingStaticinModule: i32 = 0;
-    pub static NewStaticinModule: i32 = 0; 
+    pub static EXISTING_PUB_STATIC_IN_MODULE: i32 = 0;
+    pub static NEW_PUB_STATIC_IN_MODULE: i32 = 0;
 }
