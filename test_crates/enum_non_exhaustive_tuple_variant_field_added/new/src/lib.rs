41247c792a481996a9cba1ead5919976aff2b8cd
shreyans <shreyans431@gmail.com>
2025-03-24 19:39:06 +0530

    Updated description, error message, test cases.

--- test_crates/enum_non_exhaustive_tuple_variant_field_added/new/src/lib.rs
+++ test_crates/enum_non_exhaustive_tuple_variant_field_added/new/src/lib.rs
@@ -1,6 +1,12 @@
 #![no_std]
 
-pub enum PublicEnum {
+pub enum ExhaustiveEnum {
+    #[non_exhaustive]
+    TupleVariantWithFieldAdded(i32, i64, u8),
+}
+
+#[non_exhaustive]
+pub enum NonExhaustiveEnum {
     #[non_exhaustive]
     TupleVariantWithFieldAdded(i32, i64, u8),
 }
@@ -8,5 +14,11 @@ pub enum PublicEnum {
 // Changes in a private enum should not be reported
 enum PrivateEnum {
     #[non_exhaustive]
+    TupleVariantWithFieldAdded(i32, i64, u8),
+}
+
+// This enum's variant will become exhaustive while also gaining a field.
+// It should trigger this lint.
+pub enum EnumWithNonExhaustiveVariant {
     TupleVariantWithFieldAdded(i32, i64, u8),
 }
--- test_crates/enum_non_exhaustive_tuple_variant_field_added/old/src/lib.rs
+++ test_crates/enum_non_exhaustive_tuple_variant_field_added/old/src/lib.rs
@@ -1,12 +1,25 @@
 #![no_std]
 
-pub enum PublicEnum {
+pub enum ExhaustiveEnum {
+    #[non_exhaustive]
+    TupleVariantWithFieldAdded(i32, u8),
+}
+
+#[non_exhaustive]
+pub enum NonExhaustiveEnum {
     #[non_exhaustive]
     TupleVariantWithFieldAdded(i32, u8),
 }
 
 // Changes in a private enum should not be reported
 enum PrivateEnum {
+    #[non_exhaustive]
+    TupleVariantWithFieldAdded(i32, u8),
+}
+
+// This enum's variant will become exhaustive while also gaining a field.
+// It should trigger this lint.
+pub enum EnumWithNonExhaustiveVariant {
     #[non_exhaustive]
     TupleVariantWithFieldAdded(i32, u8),
 }
