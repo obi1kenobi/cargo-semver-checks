f2906eb78e72148affad2aa5c22cdd29f94bb2f1
Shreyans <shreyans431@gmail.com>
2025-04-15 06:46:43 +0530

    Add `enum_non_exhaustive_tuple_variant_field_added` lint. (#1186)
    Addresses the following checkbox in #949 - 
    adding a new field to an existing #[non_exhaustive] tuple variant of a
    public API enum
    
    ---------
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>

--- src/query.rs
+++ src/query.rs
@@ -1236,6 +1236,7 @@ add_lints!(
     enum_no_repr_variant_discriminant_changed,
     enum_non_exhaustive_struct_variant_field_added,
     enum_non_exhaustive_tuple_variant_changed_kind,
+    enum_non_exhaustive_tuple_variant_field_added,
     enum_now_doc_hidden,
     enum_repr_int_added,
     enum_repr_int_changed,
