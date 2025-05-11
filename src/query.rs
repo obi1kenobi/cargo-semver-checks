f873768c89bf3c7d3959577cb1a5821da4bd658b
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-05-11 22:11:27 +0530

    new lint: `type_method_receiver_ref_became_owned`

--- src/query.rs
+++ src/query.rs
@@ -1368,6 +1368,7 @@ add_lints!(
     type_associated_const_marked_deprecated,
     type_marked_deprecated,
     type_method_marked_deprecated,
+    type_method_receiver_ref_became_owned,
     type_mismatched_generic_lifetimes,
     type_requires_more_const_generic_params,
     type_requires_more_generic_type_params,
