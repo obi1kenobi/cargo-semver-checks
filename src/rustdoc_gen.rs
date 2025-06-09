cbeda93f5b1351bc0308934b12062f47c2128260
Kornel <kornel@cloudflare.com>
2025-06-09 22:03:26 +0100

    Support local registry source replacement

--- src/rustdoc_gen.rs
+++ src/rustdoc_gen.rs
@@ -635,6 +635,8 @@ impl RustdocFromRegistry {
                 let client = builder.build().context("failed to build HTTP client")?;
                 index::RemoteSparseIndex::new(sparse, client).into()
             }
+            #[cfg(feature = "local-registry-replacement")]
+            ComboIndexCache::Local(local) => local.into(),
             _ => bail!("encountered unknown cache type"),
         };
 
