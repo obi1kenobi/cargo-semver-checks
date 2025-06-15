919a3a483b4e6f3c36e28a01cf4b2b4391f47b06
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2025-06-15 17:40:39 -0400

    Update test_crates/unsafe_inherent_method_target_feature_added/new/src/lib.rs

--- test_crates/unsafe_inherent_method_target_feature_added/new/src/lib.rs
+++ test_crates/unsafe_inherent_method_target_feature_added/new/src/lib.rs
@@ -9,6 +9,10 @@ impl A {
     /// Not a breaking change on x86-64 since `sse2` is enabled by default.
     #[target_feature(enable = "sse2")]
     pub unsafe fn sse2_method() {}
+    
+    // Still a breaking change, even though it becomes safe.
+    #[target_feature(enable = "avx")]
+    pub fn becomes_safe() {}
 
     #[target_feature(enable = "avx")]
     pub fn safe() {}
