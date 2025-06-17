81e9020587d699b38656ae51863264e9995b2740
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-06-17 04:04:37 +0000

    Demonstrate the breakage with witnesses.

--- test_crates/safe_function_target_feature_added/new/src/lib.rs
+++ test_crates/safe_function_target_feature_added/new/src/lib.rs
@@ -1,10 +1,24 @@
 #![no_std]
 
+/// Proof that our witness demonstrates breakage:
+/// ```compile_fail
+/// fn witness() {
+///     let _: Box<dyn Fn(_) -> _> = Box::new(safe_function_target_feature_added::compute);
+/// }
+/// ```
 #[target_feature(enable = "sse2")]
 pub fn compute(x: u32) -> u32 {
     x + 1
 }
 
+/// Proof that our witness demonstrates breakage:
+/// ```compile_fail
+/// fn witness() {
+///     let _: Box<dyn Fn(_) -> _> = Box::new(
+///         safe_function_target_feature_added::will_become_unsafe
+///     );
+/// }
+/// ```
 #[target_feature(enable = "sse2")]
 pub unsafe fn will_become_unsafe(x: u32) -> u32 {
     x / 2
