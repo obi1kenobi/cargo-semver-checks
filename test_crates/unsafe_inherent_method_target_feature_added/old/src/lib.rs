61e55037ec6132a3bef02b4b03234c9ecdbeb251
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-06-18 02:55:42 +0000

    Fix test case. Update snapshots.

--- test_crates/unsafe_inherent_method_target_feature_added/old/src/lib.rs
+++ test_crates/unsafe_inherent_method_target_feature_added/old/src/lib.rs
@@ -7,8 +7,6 @@ impl A {
 
     pub unsafe fn sse2_method() {}
 
-    // Still a breaking change, even though it becomes safe.
-    #[target_feature(enable = "avx")]
     pub unsafe fn becomes_safe() {}
 
     pub fn safe() {}
