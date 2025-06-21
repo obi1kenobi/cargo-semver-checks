e87a6b006013cf71a9bdcdab83be92c33b88eac1
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-06-21 17:48:11 +0000

    Add `pub_api_sealed_trait_method_target_feature_removed` lint.
    Also adjust the edge cases around sealed lints for it and its
    `trait_method_target_feature_removed` sibling lint.

--- test_crates/trait_method_target_feature_removed/old/src/lib.rs
+++ test_crates/trait_method_target_feature_removed/old/src/lib.rs
@@ -2,7 +2,20 @@
 
 mod private {
     pub trait Sealed {}
+
+    pub trait ReExportNoLongerSealed {}
+
+    #[doc(hidden)]
+    pub trait FromHiddenToSealed {}
 }
+
+pub use private::FromHiddenToSealed;
+
+#[doc(hidden)]
+pub trait Hidden {}
+
+#[doc(hidden)]
+pub trait NoLongerHidden {}
 
 pub trait TraitA {
     #[target_feature(enable = "avx2")]
@@ -34,7 +47,37 @@ pub trait TraitD: private::Sealed {
     unsafe fn sealed_trait_feature_removed(&self) {}
 }
 
+pub trait PublicApiSealed: Hidden {
+    #[target_feature(enable = "bmi1")]
+    unsafe fn feature_removed(&self) {}
+}
+
 trait PrivateTrait {
     #[target_feature(enable = "avx2")]
     unsafe fn private_feature_removed(&self) {}
+}
+
+pub trait NoLongerPublicApiSealed: NoLongerHidden {
+    #[target_feature(enable = "bmi1")]
+    unsafe fn feature_removed(&self) {}
+}
+
+pub trait NoLongerSealed: private::ReExportNoLongerSealed {
+    #[target_feature(enable = "bmi1")]
+    unsafe fn sealed_trait_feature_removed(&self) {}
+}
+
+pub trait FeatureRemovedAndSealed {
+    #[target_feature(enable = "bmi1")]
+    unsafe fn feature_removed(&self) {}
+}
+
+pub trait FeatureRemovedAndPubApiSealed {
+    #[target_feature(enable = "bmi1")]
+    unsafe fn feature_removed(&self) {}
+}
+
+pub trait FeatureRemovedFromPubApiSealedToUnconditional: private::FromHiddenToSealed {
+    #[target_feature(enable = "bmi1")]
+    unsafe fn feature_removed(&self) {}
 }
