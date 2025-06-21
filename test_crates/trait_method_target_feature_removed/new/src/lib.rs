ef0c5bc1ef54e3fcc7d647bdee5367f44eb26752
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2025-06-21 17:42:58 +0200

    Add trait_method_target_feature_removed lint (#1337)
    ## Summary
    - detect when a target feature requirement is removed from a trait
    method
    - test cases for target feature removal in trait methods
    - add the lint to the registry and update snapshots
    
    ## Testing
    - `cargo fmt`
    - `cargo test -- --quiet`
    
    
    ------
    https://chatgpt.com/codex/tasks/task_e_6854c92f0064832d890cfeb89f2cbb66

---
+++ test_crates/trait_method_target_feature_removed/new/src/lib.rs
@@ -0,0 +1,36 @@
+#![no_std]
+
+mod private {
+    pub trait Sealed {}
+}
+
+pub trait TraitA {
+    unsafe fn feature_removed(&self) {}
+}
+
+pub trait TraitB {
+    #[target_feature(enable = "avx2")]
+    unsafe fn still_featured(&self) {}
+}
+
+pub trait TraitE {
+    #[target_feature(enable = "bmi1")]
+    unsafe fn partial_feature_removed(&self) {}
+}
+
+pub trait TraitF {
+    #[target_feature(enable = "avx2")]
+    unsafe fn consolidated(&self) {}
+}
+
+pub trait TraitC {
+    unsafe fn remove_globally_enabled_feature(&self) {}
+}
+
+pub trait TraitD: private::Sealed {
+    unsafe fn sealed_trait_feature_removed(&self) {}
+}
+
+trait PrivateTrait {
+    unsafe fn private_feature_removed(&self) {}
+}
---
+++ test_crates/trait_method_target_feature_removed/old/src/lib.rs
@@ -0,0 +1,40 @@
+#![no_std]
+
+mod private {
+    pub trait Sealed {}
+}
+
+pub trait TraitA {
+    #[target_feature(enable = "avx2")]
+    unsafe fn feature_removed(&self) {}
+}
+
+pub trait TraitB {
+    #[target_feature(enable = "avx2")]
+    unsafe fn still_featured(&self) {}
+}
+
+pub trait TraitE {
+    #[target_feature(enable = "avx", enable = "bmi1")]
+    unsafe fn partial_feature_removed(&self) {}
+}
+
+pub trait TraitF {
+    #[target_feature(enable = "avx", enable = "avx2")]
+    unsafe fn consolidated(&self) {}
+}
+
+pub trait TraitC {
+    #[target_feature(enable = "sse2")]
+    unsafe fn remove_globally_enabled_feature(&self) {}
+}
+
+pub trait TraitD: private::Sealed {
+    #[target_feature(enable = "avx2")]
+    unsafe fn sealed_trait_feature_removed(&self) {}
+}
+
+trait PrivateTrait {
+    #[target_feature(enable = "avx2")]
+    unsafe fn private_feature_removed(&self) {}
+}
