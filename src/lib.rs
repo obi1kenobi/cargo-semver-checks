0ab98356724079c2ee561e3164faaf2d9b68c1d8
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-07-22 04:12:53 +0000

    Explicitly opt into reading unresolved manifests.

--- src/lib.rs
+++ src/lib.rs
@@ -625,8 +625,10 @@ fn overrides_for_workspace_package(
         )
     })?;
     let selected_manifest =
-        manifest::Manifest::parse(package.manifest_path.clone().into_std_path_buf())?;
-    let use_workspace_lints = selected_manifest.parsed.lints == cargo_toml::Inheritable::Inherited;
+        manifest::Manifest::parse_standalone(package.manifest_path.clone().into_std_path_buf())?;
+
+    // N.B.: Do not use `==` here, because `==` is false for inherited values.
+    let use_workspace_lints = matches!(selected_manifest.parsed.lints, cargo_toml::Inheritable::Inherited);
     let metadata_workspace_key = lint_table.as_ref().is_some_and(|x| x.workspace);
 
     let mut overrides = OverrideStack::new();
