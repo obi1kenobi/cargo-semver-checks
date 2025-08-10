a5bd102ed5deb06a79d3b4418bed42389187e24b
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-08-10 15:57:31 +0000

    Minimum supported Rust version is now 1.88.

--- src/config.rs
+++ src/config.rs
@@ -37,7 +37,7 @@ impl GlobalConfig {
         Self {
             level: None,
             handlebars: make_handlebars_registry(),
-            minimum_rustc_version: semver::Version::new(1, 87, 0),
+            minimum_rustc_version: semver::Version::new(1, 88, 0),
             stdout: AutoStream::new(Box::new(std::io::stdout()), stdout_choice),
             stderr: AutoStream::new(Box::new(std::io::stderr()), stderr_choice),
             feature_flags: HashSet::new(),
