3b26153674ab1779e049ae095ac6a1888b56fa1a
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2025-09-02 15:16:14 +1200

    Add `function_const_generic_reordered` and `function_generic_type_reordered` lints. (#1407)

---
+++ test_crates/function_generics_reordered/new/src/lib.rs
@@ -0,0 +1,15 @@
+#![no_std]
+
+pub fn one_each_type_first<const N: usize, T>(_value: [T; N]) {}
+
+pub fn one_each_const_first<T, const N: usize, >(_value: [T; N]) {}
+
+pub fn lifetime_ignored<'a, T, const N: usize>(_value: &'a [T; N]) {}
+
+pub fn two_each_but_one_changed<T, U, const N: usize, const M: usize>(_x: [T; N], _y: [U; M]) {}
+
+pub fn two_each_two_changed<const N: usize, T, U, const M: usize>(_x: [T; N], _y: [U; M]) {}
+
+pub fn like_for_like_changed_not_breaking<U, const M: usize, const N: usize, T>(_x: [U; M], _y: [T; N]) {}
+
+fn breaking_but_not_pub<const N: usize, T>(_value: [T; N]) {}
