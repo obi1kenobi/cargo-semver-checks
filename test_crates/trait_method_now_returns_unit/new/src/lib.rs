c29f794af7fcdd55001f96a17d1dfa72e11a8d7b
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-09-14 16:00:10 +0000

    Fix formatting.

--- test_crates/trait_method_now_returns_unit/new/src/lib.rs
+++ test_crates/trait_method_now_returns_unit/new/src/lib.rs
@@ -5,10 +5,15 @@ use core::future::Future;
 #[allow(async_fn_in_trait)]
 pub trait Example {
     fn was_value() -> ();
+
     async fn async_was_value();
+
     fn impl_future_to_unit() -> ();
+
     async fn impl_future_now_async();
+
     fn async_now_impl_future() -> impl Future<Output = ()>;
+
     #[doc(hidden)]
     fn doc_hidden_was_value() -> ();
 }
--- test_crates/trait_method_now_returns_unit/old/src/lib.rs
+++ test_crates/trait_method_now_returns_unit/old/src/lib.rs
@@ -5,10 +5,15 @@ use core::future::Future;
 #[allow(async_fn_in_trait)]
 pub trait Example {
     fn was_value() -> i32;
+
     async fn async_was_value() -> i32;
+
     fn impl_future_to_unit() -> impl Future<Output = ()>;
+
     fn impl_future_now_async() -> impl Future<Output = ()>;
+
     async fn async_now_impl_future();
+
     #[doc(hidden)]
     fn doc_hidden_was_value() -> i32;
 }
