752773db79f0cfb6d4476646a4cb9261eb06220a
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-11-07 18:55:17 -0500

    Fix false-positive in `trait_method_parameter_count_changed` lint.

---
+++ test_crates/trait_method_receiver_changes/new/src/lib.rs
@@ -0,0 +1,101 @@
+use std::pin::Pin;
+use std::rc::Rc;
+use std::sync::Arc;
+
+pub trait PublicTrait {
+    fn self_ref_to_refmut(&mut self);
+
+    fn box_ref_to_owned(self: Box<Self>);
+
+    fn rc_refmut_to_ref(self: &Rc<Self>);
+
+    fn rcbox_refmut_to_owned(self: Rc<Box<Self>>);
+
+    fn pin_owned_to_ref(self: &Pin<&mut Self>);
+
+    fn self_owned_to_refmut(&mut self);
+
+    fn ref_self_to_box(self: &Box<Self>);
+
+    fn refmut_self_to_pin(self: &mut Pin<&mut Self>);
+
+    fn owned_self_to_rc(self: Rc<Self>);
+
+    fn ref_to_owned_pin(self: Pin<&mut Self>);
+
+    fn refmut_box_to_ref_rcbox(self: &Rc<Box<Self>>);
+
+    fn owned_rc_to_refmut_box(self: &mut Box<Self>);
+
+    fn method_to_assoc_fn();
+}
+
+#[doc(hidden)]
+pub trait HiddenTrait {
+    fn self_ref_to_refmut(&mut self);
+
+    fn box_ref_to_owned(self: Box<Self>);
+
+    fn rc_refmut_to_ref(self: &Rc<Self>);
+
+    fn rcbox_refmut_to_owned(self: Rc<Box<Self>>);
+
+    fn pin_owned_to_ref(self: &Pin<&mut Self>);
+
+    fn self_owned_to_refmut(&mut self);
+
+    fn ref_self_to_box(self: &Box<Self>);
+
+    fn refmut_self_to_pin(self: &mut Pin<&mut Self>);
+
+    fn owned_self_to_rc(self: Rc<Self>);
+
+    fn ref_to_owned_pin(self: Pin<&mut Self>);
+
+    fn refmut_box_to_ref_rcbox(self: &Rc<Box<Self>>);
+
+    fn owned_rc_to_refmut_box(self: &mut Box<Self>);
+
+    fn method_to_assoc_fn();
+}
+
+pub trait HiddenMethods {
+    #[doc(hidden)]
+    fn self_ref_to_refmut(&mut self);
+
+    #[doc(hidden)]
+    fn box_ref_to_owned(self: Box<Self>);
+
+    #[doc(hidden)]
+    fn rc_refmut_to_ref(self: &Rc<Self>);
+
+    #[doc(hidden)]
+    fn rcbox_refmut_to_owned(self: Rc<Box<Self>>);
+
+    #[doc(hidden)]
+    fn pin_owned_to_ref(self: &Pin<&mut Self>);
+
+    #[doc(hidden)]
+    fn self_owned_to_refmut(&mut self);
+
+    #[doc(hidden)]
+    fn ref_self_to_box(self: &Box<Self>);
+
+    #[doc(hidden)]
+    fn refmut_self_to_pin(self: &mut Pin<&mut Self>);
+
+    #[doc(hidden)]
+    fn owned_self_to_rc(self: Rc<Self>);
+
+    #[doc(hidden)]
+    fn ref_to_owned_pin(self: Pin<&mut Self>);
+
+    #[doc(hidden)]
+    fn refmut_box_to_ref_rcbox(self: &Rc<Box<Self>>);
+
+    #[doc(hidden)]
+    fn owned_rc_to_refmut_box(self: &mut Box<Self>);
+
+    #[doc(hidden)]
+    fn method_to_assoc_fn();
+}
---
+++ test_crates/trait_method_receiver_changes/old/src/lib.rs
@@ -0,0 +1,101 @@
+use std::pin::Pin;
+use std::rc::Rc;
+use std::sync::Arc;
+
+pub trait PublicTrait {
+    fn self_ref_to_refmut(&self);
+
+    fn box_ref_to_owned(self: &Box<Self>);
+
+    fn rc_refmut_to_ref(self: &mut Rc<Self>);
+
+    fn rcbox_refmut_to_owned(self: &mut Rc<Box<Self>>);
+
+    fn pin_owned_to_ref(self: Pin<&mut Self>);
+
+    fn self_owned_to_refmut(self);
+
+    fn ref_self_to_box(&self);
+
+    fn refmut_self_to_pin(&mut self);
+
+    fn owned_self_to_rc(self);
+
+    fn ref_to_owned_pin(&self);
+
+    fn refmut_box_to_ref_rcbox(self: &mut Box<Self>);
+
+    fn owned_rc_to_refmut_box(self: Rc<Self>);
+
+    fn method_to_assoc_fn(&self);
+}
+
+#[doc(hidden)]
+pub trait HiddenTrait {
+    fn self_ref_to_refmut(&self);
+
+    fn box_ref_to_owned(self: &Box<Self>);
+
+    fn rc_refmut_to_ref(self: &mut Rc<Self>);
+
+    fn rcbox_refmut_to_owned(self: &mut Rc<Box<Self>>);
+
+    fn pin_owned_to_ref(self: Pin<&mut Self>);
+
+    fn self_owned_to_refmut(self);
+
+    fn ref_self_to_box(&self);
+
+    fn refmut_self_to_pin(&mut self);
+
+    fn owned_self_to_rc(self);
+
+    fn ref_to_owned_pin(&self);
+
+    fn refmut_box_to_ref_rcbox(self: &mut Box<Self>);
+
+    fn owned_rc_to_refmut_box(self: Rc<Self>);
+
+    fn method_to_assoc_fn(&self);
+}
+
+pub trait HiddenMethods {
+    #[doc(hidden)]
+    fn self_ref_to_refmut(&self);
+
+    #[doc(hidden)]
+    fn box_ref_to_owned(self: &Box<Self>);
+
+    #[doc(hidden)]
+    fn rc_refmut_to_ref(self: &mut Rc<Self>);
+
+    #[doc(hidden)]
+    fn rcbox_refmut_to_owned(self: &mut Rc<Box<Self>>);
+
+    #[doc(hidden)]
+    fn pin_owned_to_ref(self: Pin<&mut Self>);
+
+    #[doc(hidden)]
+    fn self_owned_to_refmut(self);
+
+    #[doc(hidden)]
+    fn ref_self_to_box(&self);
+
+    #[doc(hidden)]
+    fn refmut_self_to_pin(&mut self);
+
+    #[doc(hidden)]
+    fn owned_self_to_rc(self);
+
+    #[doc(hidden)]
+    fn ref_to_owned_pin(&self);
+
+    #[doc(hidden)]
+    fn refmut_box_to_ref_rcbox(self: &mut Box<Self>);
+
+    #[doc(hidden)]
+    fn owned_rc_to_refmut_box(self: Rc<Self>);
+
+    #[doc(hidden)]
+    fn method_to_assoc_fn(&self);
+}
