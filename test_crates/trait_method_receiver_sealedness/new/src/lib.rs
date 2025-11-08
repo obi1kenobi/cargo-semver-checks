f3c406c90d0c0007f81249ded8577d0190492f36
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-11-08 00:46:12 -0500

    Add remaining trait receiver lints.
    Resolves #1233.
    Resolves #1234.
    Resolves #1235.
    Resolves #1222.

---
+++ test_crates/trait_method_receiver_sealedness/new/src/lib.rs
@@ -0,0 +1,68 @@
+#![no_std]
+
+#[doc(hidden)]
+pub trait SealSuper {}
+
+mod private {
+    pub trait Sealed {}
+}
+
+// ===== Unsealed traits =====
+
+pub trait UnsealedTrait {
+    fn mut_to_ref(&self);
+    fn owned_to_ref(&self);
+    fn owned_to_mut(&mut self);
+}
+
+#[doc(hidden)]
+pub trait DocHiddenUnsealedTrait {
+    fn mut_to_ref(&self);
+    fn owned_to_ref(&self);
+    fn owned_to_mut(&mut self);
+}
+
+pub trait UnsealedTraitWithHiddenMethods {
+    #[doc(hidden)]
+    fn hidden_mut_to_ref(&self);
+
+    #[doc(hidden)]
+    fn hidden_owned_to_ref(&self);
+
+    #[doc(hidden)]
+    fn hidden_owned_to_mut(&mut self);
+}
+
+// ===== Public API sealed traits =====
+
+pub trait PublicApiSealedTrait: SealSuper {
+    fn mut_to_ref(&self);
+    fn owned_to_ref(&self);
+    fn owned_to_mut(&mut self);
+}
+
+#[doc(hidden)]
+pub trait DocHiddenPublicApiSealedTrait: SealSuper {
+    fn mut_to_ref(&self);
+    fn owned_to_ref(&self);
+    fn owned_to_mut(&mut self);
+}
+
+pub trait PublicApiSealedTraitWithHiddenMethods: SealSuper {
+    #[doc(hidden)]
+    fn hidden_mut_to_ref(&self);
+
+    #[doc(hidden)]
+    fn hidden_owned_to_ref(&self);
+
+    #[doc(hidden)]
+    fn hidden_owned_to_mut(&mut self);
+}
+
+// ===== Unconditionally sealed traits =====
+
+pub trait UnconditionallySealedTrait: private::Sealed {
+    fn mut_to_ref(&self);
+    fn owned_to_ref(&self);
+    fn owned_to_mut(&mut self);
+}
