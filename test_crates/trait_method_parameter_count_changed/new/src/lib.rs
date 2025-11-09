a317de4791da0c1923fe885d4c1d36e09ed4e225
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-11-09 11:27:39 -0500

    Match up receivers in param-count-changed lints to avoid false-positives

--- test_crates/trait_method_parameter_count_changed/new/src/lib.rs
+++ test_crates/trait_method_parameter_count_changed/new/src/lib.rs
@@ -3,5 +3,31 @@
 pub trait Example {
     fn gain_a_parameter(x: i64);
 
+    fn gain_a_parameter_with_receiver(&self, x: i64);
+
     fn lose_a_parameter();
+
+    fn lose_a_parameter_with_receiver(&self);
+
+    // This is breaking for unsealed traits, because the impl has to match.
+    fn parameter_becomes_receiver(&self);
+
+    // This is breaking for both unsealed traits (impl has to match) and in general
+    // if the method is callable, because calling via the receiver doesn't work anymore.
+    fn receiver_becomes_parameter(x: &Self);
+}
+
+mod private {
+    pub trait Sealed {}
+}
+
+pub trait SealedExample {
+    // This is breaking for unsealed traits, because the impl has to match.
+    // This trait is sealed, so this shouldn't get flagged.
+    fn parameter_becomes_receiver(&self);
+
+    // This is breaking for both unsealed traits (impl has to match) and in general
+    // if the method is callable, because calling via the receiver doesn't work anymore.
+    // It's still breaking here.
+    fn receiver_becomes_parameter(x: &Self);
 }
--- test_crates/trait_method_parameter_count_changed/old/src/lib.rs
+++ test_crates/trait_method_parameter_count_changed/old/src/lib.rs
@@ -3,5 +3,30 @@
 pub trait Example {
     fn gain_a_parameter();
 
+    fn gain_a_parameter_with_receiver(&self);
+
     fn lose_a_parameter(a: i64);
+
+    fn lose_a_parameter_with_receiver(&self, a: i64);
+
+    // This is breaking for unsealed traits, because the impl has to match.
+    fn parameter_becomes_receiver(x: &Self);
+
+    // This is breaking for both unsealed traits (impl has to match) and in general
+    // if the method is callable, because calling via the receiver doesn't work anymore.
+    fn receiver_becomes_parameter(&self);
+}
+
+mod private {
+    pub trait Sealed {}
+}
+
+pub trait SealedExample {
+    // This is breaking for unsealed traits, because the impl has to match.
+    fn parameter_becomes_receiver(x: &Self);
+
+    // This is breaking for both unsealed traits (impl has to match) and in general
+    // if the method is callable, because calling via the receiver doesn't work anymore.
+    // It's still breaking here.
+    fn receiver_becomes_parameter(&self);
 }
