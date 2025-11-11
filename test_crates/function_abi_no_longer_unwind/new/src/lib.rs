7b686d550fdb627f56a10596f6fac96335a58ddc
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-11-10 22:10:43 -0500

    Add inherent/trait method lints for becoming/losing unwind in ABI.

--- test_crates/function_abi_no_longer_unwind/new/src/lib.rs
+++ test_crates/function_abi_no_longer_unwind/new/src/lib.rs
@@ -3,3 +3,33 @@
 pub extern "C" fn unwind_function_becomes_non_unwind() {}
 
 pub extern "C-unwind" fn non_unwind_function_becomes_unwind() {}
+
+pub struct InherentUnwindToggles;
+
+impl InherentUnwindToggles {
+    pub extern "C" fn inherent_unwind_becomes_non_unwind() {}
+    pub extern "C-unwind" fn inherent_non_unwind_becomes_unwind() {}
+
+    #[doc(hidden)]
+    pub extern "C" fn doc_hidden_inherent_method_becomes_non_unwind() {}
+}
+
+#[doc(hidden)]
+pub struct DocHiddenInherentOwner;
+
+impl DocHiddenInherentOwner {
+    pub extern "C" fn doc_hidden_inherent_owner_method_becomes_non_unwind() {}
+}
+
+pub trait TraitUnwindToggles {
+    extern "C" fn trait_unwind_becomes_non_unwind();
+    extern "C-unwind" fn trait_non_unwind_becomes_unwind();
+
+    #[doc(hidden)]
+    extern "C" fn doc_hidden_trait_method_becomes_non_unwind();
+}
+
+#[doc(hidden)]
+pub trait DocHiddenTraitUnwind {
+    extern "C" fn doc_hidden_trait_becomes_non_unwind();
+}
--- test_crates/function_abi_no_longer_unwind/old/src/lib.rs
+++ test_crates/function_abi_no_longer_unwind/old/src/lib.rs
@@ -3,3 +3,33 @@
 pub extern "C-unwind" fn unwind_function_becomes_non_unwind() {}
 
 pub extern "C" fn non_unwind_function_becomes_unwind() {}
+
+pub struct InherentUnwindToggles;
+
+impl InherentUnwindToggles {
+    pub extern "C-unwind" fn inherent_unwind_becomes_non_unwind() {}
+    pub extern "C" fn inherent_non_unwind_becomes_unwind() {}
+
+    #[doc(hidden)]
+    pub extern "C-unwind" fn doc_hidden_inherent_method_becomes_non_unwind() {}
+}
+
+#[doc(hidden)]
+pub struct DocHiddenInherentOwner;
+
+impl DocHiddenInherentOwner {
+    pub extern "C-unwind" fn doc_hidden_inherent_owner_method_becomes_non_unwind() {}
+}
+
+pub trait TraitUnwindToggles {
+    extern "C-unwind" fn trait_unwind_becomes_non_unwind();
+    extern "C" fn trait_non_unwind_becomes_unwind();
+
+    #[doc(hidden)]
+    extern "C-unwind" fn doc_hidden_trait_method_becomes_non_unwind();
+}
+
+#[doc(hidden)]
+pub trait DocHiddenTraitUnwind {
+    extern "C-unwind" fn doc_hidden_trait_becomes_non_unwind();
+}
