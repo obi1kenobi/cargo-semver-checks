65e0d7ba8a6ace6cc69640bce05d9fed7b07d54a
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2025-11-10 22:18:18 -0500

    Add ABI change lints for inherent methods and trait methods. (#1508)
    - **Add ABI changed lints for inherent methods and traits.**
    - **Add inherent/trait method lints for becoming/losing unwind in ABI.**
    
    Resolves #1281.

--- test_crates/function_changed_abi/new/src/lib.rs
+++ test_crates/function_changed_abi/new/src/lib.rs
@@ -21,3 +21,41 @@ extern "Rust" fn private_c_function_becomes_rust() -> () {}
 pub extern "Rust" fn implicit_rust_function_becomes_rust() -> () {}
 
 pub fn rust_function_becomes_implicit_rust() -> () {}
+
+pub struct InherentType;
+
+impl InherentType {
+    pub extern "Rust" fn inherent_c_becomes_rust() {}
+}
+
+pub trait TraitWithAbi {
+    extern "Rust" fn trait_c_becomes_rust();
+}
+
+#[doc(hidden)]
+pub struct DocHiddenInherentType;
+
+impl DocHiddenInherentType {
+    // shouldn't trigger the lint due to #[doc(hidden)] type
+    pub extern "Rust" fn doc_hidden_inherent_c_becomes_rust() {}
+}
+
+pub struct DocHiddenInherentMethod;
+
+impl DocHiddenInherentMethod {
+    // shouldn't trigger the lint due to #[doc(hidden)] method
+    #[doc(hidden)]
+    pub extern "Rust" fn doc_hidden_inherent_c_becomes_rust() {}
+}
+
+#[doc(hidden)]
+pub trait DocHiddenTraitWithAbi {
+    // shouldn't trigger the lint due to #[doc(hidden)] type
+    extern "Rust" fn doc_hidden_trait_c_becomes_rust();
+}
+
+pub trait DocHiddenMethodInTraitWithAbi {
+    // shouldn't trigger the lint due to #[doc(hidden)] method
+    #[doc(hidden)]
+    extern "Rust" fn doc_hidden_trait_c_becomes_rust();
+}
--- test_crates/function_changed_abi/old/src/lib.rs
+++ test_crates/function_changed_abi/old/src/lib.rs
@@ -22,3 +22,41 @@ extern "C" fn private_c_function_becomes_rust() -> () {}
 pub fn implicit_rust_function_becomes_rust() -> () {}
 
 pub extern "Rust" fn rust_function_becomes_implicit_rust() -> () {}
+
+pub struct InherentType;
+
+impl InherentType {
+    pub extern "C" fn inherent_c_becomes_rust() {}
+}
+
+pub trait TraitWithAbi {
+    extern "C" fn trait_c_becomes_rust();
+}
+
+#[doc(hidden)]
+pub struct DocHiddenInherentType;
+
+impl DocHiddenInherentType {
+    // shouldn't trigger the lint due to #[doc(hidden)] type
+    pub extern "C" fn doc_hidden_inherent_c_becomes_rust() {}
+}
+
+pub struct DocHiddenInherentMethod;
+
+impl DocHiddenInherentMethod {
+    // shouldn't trigger the lint due to #[doc(hidden)] method
+    #[doc(hidden)]
+    pub extern "C" fn doc_hidden_inherent_c_becomes_rust() {}
+}
+
+#[doc(hidden)]
+pub trait DocHiddenTraitWithAbi {
+    // shouldn't trigger the lint due to #[doc(hidden)] type
+    extern "C" fn doc_hidden_trait_c_becomes_rust();
+}
+
+pub trait DocHiddenMethodInTraitWithAbi {
+    // shouldn't trigger the lint due to #[doc(hidden)] method
+    #[doc(hidden)]
+    extern "C" fn doc_hidden_trait_c_becomes_rust();
+}
