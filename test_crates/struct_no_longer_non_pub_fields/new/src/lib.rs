55d6bca5c0c22c24aa72130171332a60f301e0a2
Connor McCarthy <connor15mcc@gmail.com>
2026-01-25 13:43:26 -0500

    Add minor lint for losing all non-`pub` fields
    An example would simply be:
    
    ```diff
    pub struct MyStruct {
    - field: i32,
    + pub field: i32,
    }
    ```
    
    ctx: https://github.com/obi1kenobi/cargo-semver-checks/pull/1550/files#r2719626142

---
+++ test_crates/struct_no_longer_non_pub_fields/new/src/lib.rs
@@ -0,0 +1,24 @@
+#![no_std]
+
+pub struct MyStruct {
+    pub pub_field: i32,
+    // private_field removed - now constructible!
+}
+
+pub struct AlwaysNonPubFieldStruct {
+    pub pub_field: i32,
+    private_field: i32,
+}
+
+pub struct AlwaysPubFieldStruct {
+    pub pub_field: i32,
+}
+
+struct NonPubStruct {
+    pub pub_field: i32,
+}
+
+#[non_exhaustive]
+pub struct NonExhaustiveStruct {
+    pub pub_field: i32,
+}
