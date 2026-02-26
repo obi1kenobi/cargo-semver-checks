168edc5286498b4673b15568d7c3d5680bb86c2c
Nalin Khanna <nalinkhanna17@gmail.com>
2026-02-26 06:12:58 +0530

    non_exhaustive_struct_pub_field_added

---
+++ test_crates/non_exhaustive_struct_pub_field_added/new/src/lib.rs
@@ -0,0 +1,36 @@
+#![no_std]
+
+// Adding a new pub field to this struct will trigger this lint
+#[non_exhaustive]
+pub struct NonExhaustiveStructWithNewPubField {
+    pub a: u32,
+    pub b: u32, // should be reported
+}
+
+// Adding a private field shouldn't trigger this lint
+#[non_exhaustive]
+pub struct NonExhaustiveStructWithNewPrivateField {
+    pub a: u32,
+    b: u32, // private, should not be reported
+}
+
+// Adding a doc hidden field shouldn't trigger this lint as it is not public_api_eligible
+#[non_exhaustive]
+pub struct NonExhaustiveStructWithNewHiddenField {
+    pub a: u32,
+    #[doc(hidden)]
+    pub b: u32, // doc-hidden, should not be reported
+}
+
+// Adding a new pub field to a private struct shouldn't trigger this lint
+#[non_exhaustive]
+struct PrivateNonExhaustiveStructWithNewPubField {
+    pub a: u32,
+    pub b: u32, // private struct, should not be reported
+}
+
+// This struct is not #[non_exhaustive], so it should not trigger this lint
+pub struct ExhaustiveStruct {
+    pub a: u32,
+    pub b: u32,
+}
