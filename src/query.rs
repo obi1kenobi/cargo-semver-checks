424c78030cc7f55602460b60d3cf2ceaac29a7d3
h-wal <harsh2102agarwal@gmail.com>
2026-03-06 01:14:34 +0530

    Add inherent_method_now_const lint

--- src/query.rs
+++ src/query.rs
@@ -1706,6 +1706,7 @@ add_lints!(
     inherent_method_must_use_added,
     inherent_method_must_use_removed,
     inherent_method_no_longer_unwind,
+    inherent_method_now_const,
     inherent_method_now_doc_hidden,
     inherent_method_now_returns_unit,
     inherent_method_now_unwind,
